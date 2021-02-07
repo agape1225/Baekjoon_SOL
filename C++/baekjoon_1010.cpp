64aafdad5f7ea09b6653cfc5cfdc22bfe3751676
agape1225 <scg9268@naver.com>
2021-02-07 17:38:40 +0900

    Update baekjoon_1010.cpp

--- C++/baekjoon_1010.cpp
+++ C++/baekjoon_1010.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <cmath>
+#define SIZE 29
 
 using namespace std;
 
 int main(void) {
+
+	unsigned int ans[SIZE][SIZE] = { 0 };
+
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (i == 0) {
+				ans[i][j] = j + 1;
+			}
+			else {
+				for (int k = 0; k < j; k++)
+					ans[i][j] += ans[i - 1][k];
+			}
+		}
+	}
+
+
 	int T;
 	cin >> T;
 
 	for (int i = 0; i < T; i++) {
 		int n, m;
-		long long count = 0;
 
 		cin >> n >> m;
 
-		if (n == 1)
-			count = m;
-		else {
-			for (int j = 0; j <= m - n; j++) {
+		cout << ans[n-1][m-1] << endl;
 
-				if (j == 0) 
-					count++;
-				else
-					count += pow(n - 1,j) + 1;
-			}
-		}
-
-		cout << count << endl;
 	}
 
 	return 0;
