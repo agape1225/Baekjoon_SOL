0e063dc29831ba5960c2afa013ca0d3c10f7f2fc
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-06-22 16:00:39 +0900

    Update baekjoon_1620.cpp

--- C++/baekjoon_1620.cpp
+++ C++/baekjoon_1620.cpp
@@ -2,25 +2,35 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <utility>
 #include <algorithm>
 
 using namespace std;
 
+int binary_search(int begin, int end, int ans, vector<pair<string, int>> pocket) {
+
+	int mid = (begin + end) / 2;
+
+	return 0;
+}
+
 int main(void) {
 	int N, M;
-	vector<string> pocket;
-	
+	//vector<string> pocket;
+	vector<pair<string, int>> pocket;
 	string buff;
 	
 
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++) {
-		
+		//pair<string, int> v_buff;
 		cin >> buff;
-
-		pocket.push_back(buff);
+		
+		pocket.push_back(make_pair(buff,i));
 	}
+
+	sort(pocket.begin(), pocket.end());
 
 	for (int i = 0; i < M; i++) {
 
@@ -37,12 +47,13 @@ int main(void) {
 		//cout << buffInt << endl;
 
 		if (buffInt) {		//입력되는 값이 숫자일때
-			cout << pocket[buffInt - 1] << '\n';
+			cout << pocket[buffInt - 1].first << '\n';
 		}
 		else {				//입력되는 값이 문자열일때
 
-			auto it = find(pocket.begin(), pocket.end(), buff);
-			cout << it - pocket.begin() + 1 << '\n';
+			//auto it = find(pocket.begin(), pocket.end(), buff);
+			//auto it = lower_bound(pocket.begin(), pocket.end(), buff);
+			//cout << *lower_bound(pocket.begin(), pocket.end(), buff) << '\n';
 
 
 		}
