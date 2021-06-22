b78aa19a451af023b8d1f34a8cd7814e4b57dce0
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-06-22 16:52:24 +0900

    Update baekjoon_1620.cpp

--- C++/baekjoon_1620.cpp
+++ C++/baekjoon_1620.cpp
@@ -4,56 +4,79 @@
 #include <sstream>
 #include <utility>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
-int binary_search(int begin, int end, int ans, vector<pair<string, int>> pocket) {
 
-	int mid = (begin + end) / 2;
+int binary_search(int size, string ans, vector<pair<string, int>> pocket) {
 
-	return 0;
+	int mid;
+	int begin = 0;
+	int end = size - 1;
+	string buff;
+
+
+	while (begin <= end) {
+
+		mid = (begin + end) / 2;
+		buff = pocket[mid].first;
+
+		if (ans == buff)
+			return pocket[mid].second;
+		else if (ans < buff) {
+			end = mid - 1;
+		}
+		else {
+			begin = mid + 1;
+		}
+
+	}
+
+	return NULL;
 }
 
 int main(void) {
+
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int N, M;
-	//vector<string> pocket;
-	vector<pair<string, int>> pocket;
+
+	map<string, int> pocket;
+	vector<string> num;
+
 	string buff;
-	
 
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++) {
-		//pair<string, int> v_buff;
 		cin >> buff;
-		
-		pocket.push_back(make_pair(buff,i));
-	}
 
-	sort(pocket.begin(), pocket.end());
+		pocket[buff] = i + 1;
+		num.push_back(buff);
+	}
 
 	for (int i = 0; i < M; i++) {
 
 		int buffInt;
 		stringstream ss;
-		
-		//cout << "----------------------------" << endl;
 
 		cin >> buff;
 
 		ss.str(buff);
 		ss >> buffInt;
 
-		//cout << buffInt << endl;
 
 		if (buffInt) {		//입력되는 값이 숫자일때
-			cout << pocket[buffInt - 1].first << '\n';
+			cout << num[buffInt - 1] << '\n';
 		}
 		else {				//입력되는 값이 문자열일때
 
-			//auto it = find(pocket.begin(), pocket.end(), buff);
-			//auto it = lower_bound(pocket.begin(), pocket.end(), buff);
-			//cout << *lower_bound(pocket.begin(), pocket.end(), buff) << '\n';
+			
+			cout << pocket[buff] << '\n';
 
 
 		}
