7dfc727fdfcd352313f8754f0d2a3ebdb2eb85c1
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-06-24 23:09:04 +0900

    Create baekjoon_1931.cpp

---
+++ C++/baekjoon_1931.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(void) {
+
+	unsigned long num1;
+	unsigned long num2;
+	int size;
+
+	map<unsigned int, unsigned int> time;
+
+	cin >> size;
+
+	for (int i = 0; i < size; i++) {
+		
+		cin >> num1 >> num2;
+
+
+	}
+
+	return 0;
+}
