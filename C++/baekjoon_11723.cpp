913f56025dfa5144d6b3c616125350a35fa96cd0
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-07-01 03:02:00 +0900

    Update baekjoon_11723.cpp

--- C++/baekjoon_11723.cpp
+++ C++/baekjoon_11723.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <algorithm>
-#define SIZE 3000000
 
 using namespace std;
-
-int set[SIZE];
 
 int main(void) {
 
@@ -12,63 +8,14 @@ int main(void) {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int index = 0;
+	int x = 1;
 
-	int size, buff;
-	string input;
+	for (int i = 0; i < 20; i++) {
 
-	cin >> size;
+		cout << i << ": ";
+		x = x | (1 << i);
 
-	for (int i = 0; i < size; i++) {
-
-		//cout << i << endl;
-
-		cin >> input;
-
-		if (input == "add") {
-
-			if (!binary_search(set, set + index, buff)){
-
-				cin >> buff;
-				set[index++] = buff;
-
-				sort(set, set + index);
-
-			}
-
-
-		}
-		else if (input == "remove") {
-
-			cin >> buff;
-
-
-		}
-		else if (input == "check") {
-			cin >> buff;
-
-
-
-		}
-		else if (input == "toggle") {
-			cin >> buff;
-
-
-		}
-		else if (input == "all") {
-
-
-			for (int j = 1; j < 21; j++) {
-
-
-			}
-
-		}
-		else {				// empty
-
-
-
-		}
+		cout << x << '\n';
 	}
 
 	return 0;
