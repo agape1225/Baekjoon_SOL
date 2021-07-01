291d2b9b3b143b6766ade49ed8e2bea1cdd9bb70
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-07-02 02:29:19 +0900

    Update baekjoon_11723.cpp

--- C++/baekjoon_11723.cpp
+++ C++/baekjoon_11723.cpp
@@ -1,21 +1,82 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+bool find(int bit, int num) {
+	int tmpOr = bit | (1 << (num - 1));
+	int tmpAnd = bit & tmpOr;
+
+	//cout << tmpOr << " " << tmpAnd << " " << (int)pow(2, num) - 1  << endl;
+
+	return tmpAnd == tmpOr;
+
+}
+
 int main(void) {
+
+	//cout << (3 & 1) << endl;
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int x = 1;
+	int num = 0;
+	int size, buff;
+	string input;
 
-	for (int i = 0; i < 20; i++) {
+	cin >> size;
 
-		cout << i << ": ";
-		x = x | (1 << i);
+	for (int i = 0; i < size; i++) {
 
-		cout << x << '\n';
+		//cout << i << endl;
+
+		cin >> input;
+
+		if (input == "add") {
+			
+			cin >> buff;
+			num = num | (1 << (buff - 1));
+			//cout << num << '\n';
+
+		}
+		else if (input == "remove") {
+
+			cin >> buff;
+
+			if (find(num, buff))
+				num = num ^ (1 << (buff - 1));
+
+		}
+		else if (input == "check") {
+			cin >> buff;
+
+			if (find(num, buff))
+				cout << 1 << '\n';
+			else
+				cout << 0 << '\n';
+
+		}
+		else if (input == "toggle") {
+			cin >> buff;
+
+			if (find(num, buff))
+				num = num ^ (1 << (buff - 1));
+			else
+				num = num | (1 << (buff - 1));
+
+
+		}
+		else if (input == "all") {
+
+			num = 1048575;
+
+		}
+		else {				// empty
+
+			num = 0;
+
+		}
 	}
 
 	return 0;
