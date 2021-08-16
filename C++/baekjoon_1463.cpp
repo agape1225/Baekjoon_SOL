f1c62479134562593d8458339dd8c0ffe2561c40
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-08-16 21:44:06 +0900

    Update baekjoon_1463.cpp

--- C++/baekjoon_1463.cpp
+++ C++/baekjoon_1463.cpp
@@ -1,74 +1,47 @@
 #include <iostream>
-#define SIZE  1000000
+#include <vector>
+#define SIZE 1000001
 
 using namespace std;
 
-int arr[SIZE];
+int dp[SIZE] = { 0 };
 
 int main(void) {
+	
+	int N;
+	cin >> N;
 
-	int x;
-	cin >> x;
+	for (int i = 1; i <= N; i++) {
 
-	arr[0] = 0;
+		if(i + 1 <= 1000000){
 
-	for (int i = 1; i < SIZE; i++) {
-
-		int num1 = 1000, num2 = 1000, num3 = 1000;
-		int count = 0;
-
-		if (i < 3)
-			arr[i] = 1;
-		else {
-
-			if ((i + 1) % 3 == 0) {
-				num1 = arr[(i + 1) / 3 - 1] + 1;
-			}
-			if ((i + 1) % 2 == 0) {
-				num2 = arr[(i + 1) / 2 - 1] + 1;
-			}
-
-			num3 = arr[i - 1] + 1;
-
-			num1 = num1 > num2 ? num2 : num1;
-			num3 = num1 > num3 ? num3 : num1;
-
-			arr[i] = num3;
-
-
-			/*if (i + 1 % 6 == 0) {
-				int buff1, buff2;
-
-				buff1 = arr[(i + 1) / 3 - 1] + 1;
-				buff2 = arr[(i + 1) / 2 - 1] + 1;
-
-				num1 = buff1 > buff2 ? buff2 : buff1;
-
-
-			}
-			else {
-
-				if ((i + 1) % 3 == 0) {
-					num1 = arr[(i + 1) / 3 - 1] + 1;
-				}
-				else if ((i + 1) % 2 == 0) {
-					num1 = arr[(i + 1) / 2 - 1] + 1;
-				}
-				else {
-					num1 = arr[i - 1] + 1;
-				}
-
-			}
-
-			num2 = arr[i - 1] + 1;
-
-			arr[i] = num1 > num2 ? num2 : num1;*/
+			if (dp[i + 1] == 0)
+				dp[i + 1] = dp[i] + 1;
+			else if (dp[i + 1] > dp[i] + 1)
+				dp[i + 1] = dp[i] + 1;
 
 		}
+
+
+		if (i * 2 < 1000000) {
+			if (dp[i * 2] == 0)
+				dp[i * 2] = dp[i] + 1;
+			else if (dp[i * 2] > dp[i] + 1)
+				dp[i * 2] = dp[i] + 1;
+		}
+
+		if (i * 3 < 1000000) {
+			if (dp[i * 3] == 0)
+				dp[i * 3] = dp[i] + 1;
+			else if (dp[i * 3] > dp[i] + 1)
+				dp[i * 3] = dp[i] + 1;
+		}
+
+
 	}
 
-	cout << arr[x - 1] << endl;
-	
+	cout << dp[N];
 
 	return 0;
+
 }
