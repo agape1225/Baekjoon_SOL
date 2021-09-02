8e5594ddf17750a00cd091b8538a57dd4c88b552
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-09-02 21:03:00 +0900

    Update baekjoon_2206.cpp

--- C++/baekjoon_2206.cpp
+++ C++/baekjoon_2206.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
+#include <queue>
 #include <string>
+#include <utility>
 
 #define SIZE 1000
-
-
+#define MAX 987654321
 
 using namespace std;
 
+int N, M;
+
 int map[SIZE][SIZE];
+int ans[SIZE][SIZE] = { 0 };
+//bool visited[SIZE][SIZE] = { 0 };
+
+typedef struct point {
+
+	int x;
+	int y;
+	int count;
+	bool is_pass;
+
+}point;
+
+queue<point> q;
+
+int dir_x[4] = { 1,0,-1,0 };
+int dir_y[4] = { 0,1,0,-1 };
+
+bool check_range(int x, int y) {
+
+	if (x < 0 || x >= N)
+		return false;
+	else {
+		if (y < 0 || y >= M)
+			return false;
+		else
+			return true;
+	}
+
+}
 
 int main(void) {
 
-	int N, M;
-	
 	string buff;
+
 
 	cin >> N >> M;
 
@@ -24,22 +55,89 @@ int main(void) {
 		for (int j = 0; j < M; j++) {
 
 			map[i][j] = buff[j] - '0';
+			ans[i][j] = MAX;
 
 		}
 
 	}
 
-	for (int i = 0; i < N; i++) {
+	point start = { 0,0,1, false };
 
-		for (int j = 0; j < M; j++) {
+	q.push(start);
 
-			cout << map[i][j] << ' ';
+	while (!q.empty()) {
+
+		point tmp = q.front();
+		q.pop();
+
+		/*if (visited[tmp.x][tmp.y]) {
+
+			if (ans[tmp.x][tmp.y] < tmp.count) {
+
+				continue;
+
+			}
+
+		}*/
+
+		//visited[tmp.x][tmp.y] = true;
+
+
+		if (ans[tmp.x][tmp.y] < tmp.count) {
+
+			continue;
 
 		}
 
-		cout << endl;
+
+		ans[tmp.x][tmp.y] = tmp.count;
+
+		for (int i = 0; i < 4; i++) {
+
+			int buff_x = tmp.x + dir_x[i];
+			int buff_y = tmp.y + dir_y[i];
+
+			if (check_range(buff_x, buff_y)) {
+
+				point p_tmp;
+				p_tmp.x = buff_x;
+				p_tmp.y = buff_y;
+				p_tmp.count = tmp.count + 1;
+
+				if (map[buff_x][buff_y] == 1) {
+
+					if (tmp.is_pass) {
+						continue;
+					}
+					else {
+						p_tmp.is_pass = true;
+					}
+
+				}
+
+				q.push(p_tmp);
+
+			}
+
+		}
+
 
 	}
+
+	//for (int i = 0; i < N; i++) {
+
+	//	for (int j = 0; j < M; j++) {
+
+	//		cout << ans[i][j] << ' ';
+
+	//	}
+	//	cout << endl;
+	//}
+
+	if (ans[N - 1][M - 1] >= MAX)
+		cout << -1;
+	else
+		cout << ans[N - 1][M - 1];
 
 	return 0;
 
