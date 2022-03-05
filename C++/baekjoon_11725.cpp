958a62bb66bf743630d452d8ff233e8fe3038ce9
agape1225 <49804691+agape1225@users.noreply.github.com>
2022-03-05 17:58:09 +0900

    Update baekjoon_11725.cpp

--- C++/baekjoon_11725.cpp
+++ C++/baekjoon_11725.cpp
@@ -1,44 +1,12 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
 
-#define SIZE 100001
-
 using namespace std;
 
-vector<int> tree[SIZE];
-int result[SIZE] = { 0 };
-bool visited[SIZE] = { 0 };
-queue<int> q;
-
-
-void DFS() {
-
-	/*ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);*/
-
-	q.push(1);
-	visited[1] = true;
-
-	while (!q.empty()) {
-
-		int buff = q.front();
-		q.pop();
-
-		for (auto it = tree[buff].begin(); it != tree[buff].end(); it++) {
-
-			if (!visited[*it]) {
-				q.push(*it);
-				result[*it] = buff;
-				visited[*it] = true;
-			}
-
-		}
-
-	}
-
-}
+vector<int> graph[100001];
+int ans[100001] = { 0 };
 
 int main(void) {
 
@@ -46,26 +14,46 @@ int main(void) {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N;
-
+	int N = NULL;
+	int root = 1;
+	queue<int> q;
 	cin >> N;
 
-	int buff1, buff2;
-
 	for (int i = 0; i < N - 1; i++) {
+		int buff1, buff2;
 
 		cin >> buff1 >> buff2;
-		tree[buff1].push_back(buff2);
-		tree[buff2].push_back(buff1);
 
+		graph[buff1].push_back(buff2);
+		graph[buff2].push_back(buff1);
 	}
 
-	DFS();
+	for (int i = 1; i <= N; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+
+	q.push(1);
+	ans[1] = 1;
+
+	while (!q.empty()) {
+
+		int buff = q.front();
+		q.pop();
+
+		for (auto it = graph[buff].begin(); it != graph[buff].end(); it++) {
+
+			if (ans[*it] == 0) {
+
+				q.push(*it);
+				ans[*it] = buff;
+
+			}
+		}
+	}
 
 	for (int i = 2; i <= N; i++) {
-		cout << result[i] << '\n';
+		cout << ans[i] << '\n';
 	}
 
 	return 0;
-
 }
