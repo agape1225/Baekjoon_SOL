98dd49b53b90f266e680c07347cd93f43f81abaf
agape1225 <49804691+agape1225@users.noreply.github.com>
2022-06-19 14:25:07 +0900

    Update baekjoon_1436.cpp

--- C++/baekjoon_1436.cpp
+++ C++/baekjoon_1436.cpp
@@ -1,34 +1,41 @@
 /******************************************************************************
+
 Welcome to GDB Online.
 GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
 C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
+
 *******************************************************************************/
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-    int N;
-    long long startNum = 666;
     
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long N;
+    int arr[10001] = {0};
+    int buff;
     cin >> N;
     
-    while(true){
-        string buff = to_string(startNum);
-        if(buff.find("666") != string::npos){
-            N--;
-        }
-        
-        startNum++;
-        
-        if(N == 0){
-            cout << buff;
-            break;
+    for(int i = 0; i < N; i++){
+        cin >> buff;
+        arr[buff]++;
+    }
+    
+    for(int i = 0; i < 10001; i++){
+        if(arr[i]){
+            int tmp = arr[i];
+            for(int j = 0; j < tmp; j++){
+                cout << i << '\n';
+            }
         }
     }
+    
 
     return 0;
 }
