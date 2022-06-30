0f0aad705a26979904d02ec7eb2f662f8ac832e6
agape1225 <49804691+agape1225@users.noreply.github.com>
2022-06-30 20:19:10 +0900

    Create baekjoon_11286.cpp

---
+++ C++/baekjoon_11286.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+struct Compare {
+	bool operator() (const long &a, const long &b) const{
+		  long buffA = a;
+      long buffB = b;
+      if(a < 0){
+        buffA = a * - 1;
+      }
+
+      if(b < 0)
+        buffB = b * -1;
+
+      if(buffA == buffB)
+        return a < b;
+      else
+        return buffA < buffB;
+	}
+};
+
+
+
+int main(void){
+
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int N;
+  long input;
+  set<long, Compare> s;
+  
+  cin >> N;
+
+  for(int t = 0; t < N; t++){
+    
+    cin >> input;
+    if(input != 0){
+
+      s.insert(input);
+
+      /*for(auto it = s.begin(); it != s.end(); it++){
+        cout << *it << ' ';
+      }
+      cout << endl;*/
+    }else{
+
+      //cout << "It's zero!" << endl;
+
+      if(s.empty())
+        cout << 0 << '\n';
+      else{
+        cout << *s.begin() << endl; 
+        s.erase(s.begin());
+      }
+    }
+      
+  }
+
+  return 0;
+}
