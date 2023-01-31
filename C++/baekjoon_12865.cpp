2ecf27a15f066b5fab2123ff64590cfb82d53b5d
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-01-31 23:32:09 +0900

    Update baekjoon_12865.cpp

--- C++/baekjoon_12865.cpp
+++ C++/baekjoon_12865.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
-#include <vector>
 #include <utility>
-
-#define SIZE 100001
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int dp[SIZE];
+int cache[101][100001] = { 0 };
+vector<pair<int, int>> stuff;
 
 int main(void) {
 
-	vector<pair<int, int>> bag;
 	int N, K;
 	int buff1, buff2;
-
 	cin >> N >> K;
 
 	for (int i = 0; i < N; i++) {
-		
 		cin >> buff1 >> buff2;
-		bag.push_back(make_pair(buff1, buff2));
+		stuff.push_back(make_pair(buff1, buff2));
+	}
 
+	for (int i = 1; i <= N; i++) {
+		pair<int, int> buff = stuff[i - 1];
+		int W, V;
+		W = buff.first;
+		V = buff.second;
+
+		for (int j = 1; j <= K; j++) {
+
+			if (j - W >= 0) {
+				cache[i][j] = max(cache[i - 1][j], cache[i - 1][j - W] + V);
+			}
+			else {
+				cache[i][j] = cache[i - 1][j];
+			}
+		}
 	}
 
 
+	cout << cache[N][K];
+
+	return 0;
 }
