01f363278d6826a230d59a3e4e447198cdd7d618
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-01-31 19:59:24 +0900

    Update baekjoon_9251.cpp

--- C++/baekjoon_9251.cpp
+++ C++/baekjoon_9251.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -8,36 +8,34 @@ int cache[1001][1001] = { 0 };
 
 int main(void) {
 
-	string s1;
-	string s2;
-	
+	string s1, s2;
+
 	cin >> s1 >> s2;
 
 	int s1Size = s1.size();
 	int s2Size = s2.size();
 
-	/*for (int i = 0; i <= s1Size; i++)
-		cache[0][i] = 0;
-
-	for (int i = 0; i <= s2Size; i++)
-		cache[i][0] = 0;*/
-
 	for (int i = 1; i <= s2Size; i++) {
-	
-		char standard = s2[i - 1];
-		
+
+		char char2 = s2[i - 1];
+
 		for (int j = 1; j <= s1Size; j++) {
 
-			if (standard == s1[j - 1]) {
+			char char1 = s1[j - 1];
+
+			if (char2 == char1) {
 
 				cache[i][j] = cache[i - 1][j - 1] + 1;
 
 			}
 			else {
-				cache[i][j] = max(cache[i][j - 1], cache[i - 1][j]);
+
+				cache[i][j] = max(cache[i - 1][j], cache[i][j - 1]);
+
 			}
 
 		}
+
 	}
 
 	cout << cache[s2Size][s1Size];
