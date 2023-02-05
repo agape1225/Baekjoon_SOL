8e63a9e51512300e2791f97d55c5df90524b2858
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-02-05 13:08:54 +0900

    Update baekjoon_11404.cpp

--- C++/baekjoon_11404.cpp
+++ C++/baekjoon_11404.cpp
@@ -1,48 +1,55 @@
 #include <iostream>
-#include <vector>
-#include <utility>
+#include <algorithm>
+
 #define INF 987654321
 
 using namespace std;
 
-int n;
-int m;
-//vector<pair<int, int>> bus[101];
-int graph[101][101] = {0};
+int graph[101][101];
 
 int main(void) {
 
-	int buff1, buff2, buff3;
-
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++)
-			graph[i][j] = INF;
-	}
-
+	int n, m;
 	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		cin >> buff1 >> buff2 >> buff3;
-		//bus[buff1].push_back(make_pair(buff2, buff3));
-		if(graph[buff1][buff2] == INF)
-			graph[buff1][buff2] = buff3;
-		else {
-			if (buff3 < graph[buff1][buff2])
-				graph[buff1][buff2] = buff3;
+	int start, end, weight;
+
+	//intializing
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			graph[i][j] = INF;
 		}
 	}
 
-	for (int i = 1; i <= n; i++) {		//i를 거쳐감
-		for (int j = 1; j <= n; j++) {
-			for (int k = 1; k <= n; k++) {
-				graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k]);
-			}
+	for (int i = 0; i < m; i++) {
+		cin >> start >> end >> weight;
+		graph[start][end] = min(graph[start][end], weight);
+	}
 
+	for (int middle = 1; middle <= n; middle++) {
+		for (int start = 1; start <= n; start++) {
+			for (int end = 1; end <= n; end++) {
+
+				if(start != end)
+					graph[start][end] = min(graph[start][middle] + graph[middle][end], graph[start][end]);
+
+				/*cout << start << ' ' << middle << ' ' << end << endl;
+				for (int i = 1; i <= n; i++) {
+					for (int j = 1; j <= n; j++) {
+						cout << graph[i][j] << ' ';
+					}
+					cout << endl;
+				}
+				cout << endl;*/
+			}
 		}
+
+
+
 	}
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
-			if (graph[i][j] >= INF || i == j)
+			if (graph[i][j] >= INF)
 				cout << 0 << ' ';
 			else
 				cout << graph[i][j] << ' ';
@@ -51,8 +58,4 @@ int main(void) {
 	}
 
 	return 0;
-
-
-
-
 }
