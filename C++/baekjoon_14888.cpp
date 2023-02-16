7e596c838d78faf1edc0d02efc7a4d2f4ce239c4
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-02-17 00:21:11 +0900

    Update baekjoon_14888.cpp

--- C++/baekjoon_14888.cpp
+++ C++/baekjoon_14888.cpp
@@ -2,15 +2,55 @@
 
 using namespace std;
 
-int sol(int count) {
+int maxNum = -987654321;
+int minNum = 987654321;
 
+int N;
+int fac = 1;
+int sufix[11] = { 0 };
+int operators[4] = { 0 };
+
+void sol(int count, int num) {
+
+	if (count == N) {
+		
+		if (num > maxNum) {
+			maxNum = num;
+		}
+
+		if (num < minNum) {
+			minNum = num;
+		}
+	}
+	else {
+
+		for (int i = 0; i < 4; i++) {
+
+			if (operators[i] > 0) {
+				operators[i]--;
+
+				if (i == 0) {			// µ¡¼À
+					sol(count + 1, num + sufix[count]);
+				}
+				else if (i == 1) {		//»¬¼À
+					sol(count + 1, num - sufix[count]);
+				}
+				else if (i == 2) {		//°ö¼À
+					sol(count + 1, num * sufix[count]);
+				}
+				else {					//³ª´°¼À
+					sol(count + 1, num / sufix[count]);
+				}
+				operators[i]++;
+			}
+		}
+
+		
+	}
 }
 
 int main(void) {
-	int N;
-	int fac = 1;
-	int sufix[11] = { 0 };
-	int operators[4] = { 0 };
+	
 
 	cin >> N;
 	for (int i = 0; i < N; i++) {
@@ -25,6 +65,11 @@ int main(void) {
 		fac *= i;
 	}*/
 
-	
+	sol(1, sufix[0]);
+
+	cout << maxNum << endl;
+	cout << minNum << endl;
+
+	return 0;
 
 }
