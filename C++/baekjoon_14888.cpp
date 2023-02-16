15fb83b5de8e5bb71b7afe213f5c37af21e95d50
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-02-16 23:54:15 +0900

    Create baekjoon_14888.cpp

---
+++ C++/baekjoon_14888.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int sol(int count) {
+
+}
+
+int main(void) {
+	int N;
+	int fac = 1;
+	int sufix[11] = { 0 };
+	int operators[4] = { 0 };
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> sufix[i];
+	}
+
+	for (int i = 0; i < 4; i++) {
+		cin >> operators[i];
+	}
+
+	/*for (int i = 1; i < N; i++) {
+		fac *= i;
+	}*/
+
+	
+
+}
