5e2d5614d3f885305015dc61ec127bb2583310d2
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-02-21 20:30:11 +0900

    Update baekjoon_2206.cpp

--- C++/baekjoon_2206.cpp
+++ C++/baekjoon_2206.cpp
@@ -3,142 +3,71 @@
 #include <string>
 #include <utility>
 
-#define SIZE 1000
-#define MAX 987654321
-
 using namespace std;
+
+int map[1000][1000];
+int visited[1000][1000][2] = { 0 };
+
+int dir_row[4] = { 0, 0, 1, -1 };
+int dir_col[4] = { 1, -1, 0, 0 };
 
 int N, M;
 
-int map[SIZE][SIZE];
-int ans[SIZE][SIZE] = { 0 };
-//bool visited[SIZE][SIZE] = { 0 };
-
-typedef struct point {
-
-	int x;
-	int y;
-	int count;
-	bool is_pass;
-
-}point;
-
-queue<point> q;
-
-int dir_x[4] = { 1,0,-1,0 };
-int dir_y[4] = { 0,1,0,-1 };
-
-bool check_range(int x, int y) {
-
-	if (x < 0 || x >= N)
+bool check_range(int row, int col) {
+	if (row < 0 || row >= N || col < 0 || col >= M)
 		return false;
-	else {
-		if (y < 0 || y >= M)
-			return false;
-		else
-			return true;
-	}
-
+	return true;
 }
 
 int main(void) {
 
-	string buff;
-
-
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++) {
-
+		string buff;
 		cin >> buff;
-
 		for (int j = 0; j < M; j++) {
-
 			map[i][j] = buff[j] - '0';
-			ans[i][j] = MAX;
-
 		}
-
 	}
 
-	point start = { 0,0,1, false };
-
-	q.push(start);
+	queue<pair<int, pair<int, int>>> q;
+	visited[0][0][1] = 1;
+	q.push(make_pair(0, make_pair(0, 1)));
 
 	while (!q.empty()) {
-
-		point tmp = q.front();
+		int row = q.front().first;
+		int col = q.front().second.first;
+		int wall = q.front().second.second;
 		q.pop();
 
-		/*if (visited[tmp.x][tmp.y]) {
-
-			if (ans[tmp.x][tmp.y] < tmp.count) {
-
-				continue;
-
-			}
-
-		}*/
-
-		//visited[tmp.x][tmp.y] = true;
-
-
-		if (ans[tmp.x][tmp.y] < tmp.count) {
-
-			continue;
-
+		if (row == N - 1 && col == M - 1) {
+			cout << visited[row][col][wall];
+			return 0;
 		}
 
-
-		ans[tmp.x][tmp.y] = tmp.count;
-
 		for (int i = 0; i < 4; i++) {
+			int row_buff = row + dir_row[i];
+			int col_buff = col + dir_col[i];
 
-			int buff_x = tmp.x + dir_x[i];
-			int buff_y = tmp.y + dir_y[i];
+			if (check_range(row_buff, col_buff)) {
 
-			if (check_range(buff_x, buff_y)) {
-
-				point p_tmp;
-				p_tmp.x = buff_x;
-				p_tmp.y = buff_y;
-				p_tmp.count = tmp.count + 1;
-
-				if (map[buff_x][buff_y] == 1) {
-
-					if (tmp.is_pass) {
-						continue;
-					}
-					else {
-						p_tmp.is_pass = true;
-					}
+				if (map[row_buff][col_buff] == 0 && visited[row_buff][col_buff][wall] == 0) {
+					visited[row_buff][col_buff][wall] = visited[row][col][wall] + 1;
+					q.push(make_pair(row_buff, make_pair(col_buff, wall)));
 
 				}
 
-				q.push(p_tmp);
+				if (map[row_buff][col_buff] == 1 && wall > 0) {
+					visited[row_buff][col_buff][wall - 1] = visited[row][col][wall] + 1;
+					q.push(make_pair(row_buff, make_pair(col_buff, wall - 1)));
+				}
 
 			}
 
 		}
-
-
 	}
 
-	//for (int i = 0; i < N; i++) {
-
-	//	for (int j = 0; j < M; j++) {
-
-	//		cout << ans[i][j] << ' ';
-
-	//	}
-	//	cout << endl;
-	//}
-
-	if (ans[N - 1][M - 1] >= MAX)
-		cout << -1;
-	else
-		cout << ans[N - 1][M - 1];
-
+	cout << -1;
 	return 0;
-
 }
