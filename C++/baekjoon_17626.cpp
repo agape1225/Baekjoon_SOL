cce7617b2dfd41ce86ee47f5a025e0a72839d15d
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-03-06 21:36:31 +0900

    Create baekjoon_17626.cpp

---
+++ C++/baekjoon_17626.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 987654321
+
+using namespace std;
+
+int cache[500001] = { 0 };
+
+int main(void) {
+
+	int n;
+	cin >> n;
+
+	cache[1] = 1;
+
+
+}
