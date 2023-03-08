031892b6ebf473288f0695b93cc4d4e50c7ffe6c
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-03-08 21:18:42 +0900

    Update baekjoon_17626.cpp

--- C++/baekjoon_17626.cpp
+++ C++/baekjoon_17626.cpp
@@ -13,5 +13,18 @@ int main(void) {
 
 	cache[1] = 1;
 
+	for (int i = 2; i <= n; i++) {
+
+		int min_num = MAX;
+
+		for (int j = 1; j * j <= i; j++) {
+			int tmp = i - j * j;
+			min_num = min(min_num, cache[tmp]);
+		}
+		cache[i] = min_num + 1;
+	}
+
+	cout << cache[n];
+	return 0;
 
 }
