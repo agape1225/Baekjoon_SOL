7f0d0c4bad0b314ef658550a9b62c71506758a03
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-03-09 14:53:30 +0900

    Update baekjoon_17626.cpp

--- C++/baekjoon_17626.cpp
+++ C++/baekjoon_17626.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 #include <algorithm>
-#define MAX 987654321
+#define MAX_NUM 987654321
 
 using namespace std;
 
-int cache[500001] = { 0 };
+int cache[50001] = { 0 };
 
 int main(void) {
 
 	int n;
 	cin >> n;
 
+	/*for (int i = 1; i <= n; i++) {
+		cache[i] = MAX_NUM;
+	}*/
+
+	//cache[0] = 0;
 	cache[1] = 1;
+	//cache[2] = 2;
 
 	for (int i = 2; i <= n; i++) {
-
-		int min_num = MAX;
+		
+		int min_num = MAX_NUM;
 
 		for (int j = 1; j * j <= i; j++) {
-			int tmp = i - j * j;
-			min_num = min(min_num, cache[tmp]);
+			int buff = i - j * j;
+			min_num = min(min_num, cache[buff]);
 		}
 		cache[i] = min_num + 1;
 	}
 
 	cout << cache[n];
+
 	return 0;
 
 }
