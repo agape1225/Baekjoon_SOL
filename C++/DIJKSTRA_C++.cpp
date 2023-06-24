bce5c3f4ba840c39edd95f1db2f01b987e251c5d
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-06-24 19:51:17 +0900

    Update DIJKSTRA_C++.cpp

--- C++/DIJKSTRA_C++.cpp
+++ C++/DIJKSTRA_C++.cpp
@@ -1,9 +1,13 @@
+/*
+* 테스트 링크: https://www.acmicpc.net/problem/1753
+* 왠만하면 우선순위 큐 사용
+*/
+
 #include <iostream>
 #include <queue>
 
 #define INF 987654321
 #define MAX_NODE 20001
-
 
 using namespace std;
 
@@ -12,34 +16,32 @@ int ans[MAX_NODE];
 
 int V, E;
 
-
 void dijkstra(int start) {
-
 	priority_queue<pair<int, int>> pq;
-	pq.push(make_pair(0, start));
 	ans[start] = 0;
+	pq.push(make_pair(0, start));
 
 	while (!pq.empty()) {
-		int current_node = pq.top().second;
-		int current_distance = -pq.top().first;
+		int current = pq.top().second;
+		int distance = -pq.top().first;
 		pq.pop();
 
-		cout << current_node << ' ' << current_distance << endl;
-
-		for (int i = 0; i < graph[current_node].size(); i++) {
-			int next_node = graph[current_node][i].first;
-			int next_distance = current_distance + (graph[current_node][i].second);
-
-			if (next_distance < ans[next_node]) {
-				ans[next_node] = next_distance;
-				pq.push(make_pair(-next_distance, next_node));
+		for (int i = 0; i < graph[current].size(); i++) {
+			int next = graph[current][i].first;
+			int next_distance = graph[current][i].second + distance;
+			if (ans[next] > next_distance) {
+				ans[next] = next_distance;
+				pq.push(make_pair(-next_distance, next));
 			}
 		}
 	}
-
 }
 
 int main(void) {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	int start;
 
