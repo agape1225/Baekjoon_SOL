3a49cd1335f19775f9d7734818205431c50d55da
agape1225 <scg9268@gmail.com>
2023-07-05 17:18:34 +0900

    Update baekjoon_10830.cpp

--- C++/baekjoon_10830.cpp
+++ C++/baekjoon_10830.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
+
 using namespace std;
 
-long long n, b;
-int arr[5][5];
+int N;
+long long B;
+
+int matrix[5][5];
 int result[5][5];
-int temp[5][5];
 
-void solved(int v1[5][5], int v2[5][5]) {
-
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) {
-			temp[i][j] = 0;
-			for (int k = 0; k < n; k++)
-				temp[i][j] += (v1[i][k] * v2[k][j]);
-			temp[i][j] %= 1000;
+void cal_matrix(int matrix1[5][5], int matrix2[5][5]) {
+	
+	int tmp[5][5] = { 0 };
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int ans = 0;
+			for (int k = 0; k < N; k++) {
+				ans += matrix1[i][k] * matrix2[k][j];
+			}
+			tmp[i][j] = ans % 1000;
 		}
+	}
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			v1[i][j] = temp[i][j];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			matrix1[i][j] = tmp[i][j];
+		}
+	}
+	
 }
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
 
-	cin >> n >> b;
+int main(void) {
+	cin >> N >> B;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> arr[i][j];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> matrix[i][j];	
 		}
 		result[i][i] = 1;
 	}
 
-	while (b) {
-		if (b % 2 == 1) {
-			solved(result, arr);
+	while (B) {
+		if (B % 2) {
+			cal_matrix(result, matrix);
 		}
-		solved(arr, arr);
-		b /= 2;
-
+		cal_matrix(matrix, matrix);
+		B = B / 2;
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			cout << result[i][j] << ' ';
+		}
 		cout << endl;
 	}
-
 	return 0;
 }
