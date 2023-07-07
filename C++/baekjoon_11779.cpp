6ff8b631266d4a34498bc10947d8e34051ec2436
agape1225 <scg9268@gmail.com>
2023-07-07 17:02:36 +0900

    Update baekjoon_11779.cpp

--- C++/baekjoon_11779.cpp
+++ C++/baekjoon_11779.cpp
@@ -8,14 +8,29 @@ using namespace std;
 
 vector<pair<int, int>> graph[1003];
 int n, m;
+
+//버스 요금
 int fare[1003];
+//경로 역추적
 int visit[1003];
 int start_node, end_node;
-vector<int> ans_num;
+
+void print_node(int node, int count) {
+	if (start_node == node) {
+		cout << count << endl;
+		//cout << visit[node] << ' ';
+	}
+	else {
+
+		print_node(visit[node], count + 1);
+		cout << visit[node] << ' ';
+	}
+}
 
 void dijkstra(int start) {
 	priority_queue<pair<int, int>> pq;
 	pq.push(make_pair(0, start));
+	visit[start] = start;
 	fare[start] = 0;
 
 	while (!pq.empty()) {
@@ -23,19 +38,21 @@ void dijkstra(int start) {
 		int current_fare = -pq.top().first;
 		pq.pop();
 
-		if (current_node == end_node) break;
 		if (fare[current_node] < current_fare) continue;
 
 		for (int i = 0; i < graph[current_node].size(); i++) {
 			int next_node = graph[current_node][i].first;
 			int next_fare = graph[current_node][i].second + current_fare;
 
-			if (next_fare < fare[next_node]) {
-				visit[next_node] = current_node;
+			
+
+			if (fare[next_node] > next_fare) {
 				fare[next_node] = next_fare;
+				visit[next_node] = current_node;
 				pq.push(make_pair(-next_fare, next_node));
 			}
 		}
+
 	}
 }
 
@@ -60,15 +77,11 @@ int main(void) {
 
 	init_fare();
 	dijkstra(start_node);
-	int t = end_node;
-	while (t) {
-		ans_num.push_back(t);
-		t = visit[t];
-	}
+	
 
 	cout << fare[end_node] << endl;
-	cout << ans_num.size() << "\n";
-	for (int i = ans_num.size() - 1; i >= 0; i--) cout << ans_num[i] << " ";
+	print_node(end_node, 1);
+	cout << end_node;
 
 	return 0;
 
