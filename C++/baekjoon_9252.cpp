5456052d950eae6676be9d65b0a6a60e93f20b9f
agape1225 <scg9268@gmail.com>
2023-07-17 22:57:17 +0900

    Update baekjoon_9252.cpp

--- C++/baekjoon_9252.cpp
+++ C++/baekjoon_9252.cpp
@@ -1,70 +1,64 @@
 #include <iostream>
-#include <string>
-#include <utility>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-pair<int, string> cache[1001][1001];
+int cache[1001][1001] = { 0 };
 
 int main(void) {
 	string str1;
 	string str2;
-	/*int str1_size;
-	int str2_size;*/
 
-	cin >> str1 >> str2;
+	int index_row, index_col;
+	vector<char> ans;
 	
-	cache[0][0] = make_pair(0, NULL);
-
-	for (int i = 0; i < str1.size(); i++) {
-		cache[0][i + 1] = make_pair(0, str1[i]);
-	}
-
-	for (int i = 0; i < str2.size(); i++) {
-		cache[i + 1][0] = make_pair(0, str2[i]);
-	}
+	cin >> str1 >> str2;
 
 	for (int i = 1; i <= str2.size(); i++) {
 		for (int j = 1; j <= str1.size(); j++) {
-			if (str2[i] == str1[j]) {
-				
-				int count = cache[i - 1][j - 1].first;
-				string prev = cache[i - 1][j - 1].second;
-				
-				if (count == 0) {
-					cache[i][j] = make_pair(count + 1, str2[i]);
-				}
-				else {
 
-					cache[i][j] = make_pair(count + 1, prev + str2[i]);
-				}
+			if (str1[j - 1] == str2[i - 1]) {
+				cache[i][j] = cache[i - 1][j - 1] + 1;
 			}
 			else {
-				int count1 = cache[i - 1][j].first;
-				string prev1 = cache[i - 1][j].second;
-				int count2 = cache[i][j - 1].first;
-				string prev2 = cache[i][j - 1].second;
-
-				if (count1 < count2) {
-					cache[i][j] = make_pair(count2, prev2);
-				}
-				else {
-					cache[i][j] = make_pair(count1, prev1);
-				}
+				cache[i][j] = max(cache[i][j - 1], cache[i - 1][j]);
 			}
+
 		}
 	}
 
-	for (int i = 0; i <= str2.size(); i++) {
+	/*for (int i = 0; i <= str2.size(); i++) {
 		for (int j = 0; j <= str1.size(); j++) {
-			cout << cache[i][j].first << ' ' << cache[i][j].second << '|';
+			cout << cache[i][j] << ' ';
 		}
 		cout << endl;
+	}*/
+
+	
+	index_row = str2.size();
+	index_col = str1.size();
+
+	while (cache[index_row][index_col]) {
+
+		if (cache[index_row][index_col] == cache[index_row - 1][index_col])
+			index_row--;
+		
+		else if (cache[index_row][index_col] == cache[index_row][index_col - 1])
+			index_col--;
+		
+		else {
+			ans.push_back(str2[index_row - 1]);
+			index_row--;
+			index_col--;
+		}
 	}
 
-	/*if(cache[str2.size()][str1.size()].first)
-		cout << cache[str2.size()][str1.size()].second;*/
+	cout << cache[str2.size()][str1.size()] << endl;
+
+	for (int i = ans.size() - 1; i >= 0; i--) {
+		cout << ans[i];
+	}
 
 	return 0;
-
 }
