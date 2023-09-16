25fd3bf9d80f6729864a5230e42af76cd2c4faf6
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-09-16 16:38:33 +0900

    Update programmers_12905.cpp

--- C++/programmers_12905.cpp
+++ C++/programmers_12905.cpp
@@ -8,16 +8,13 @@ bool check_square(vector<vector<int>> board, int start_row, int start_col, int s
     int index_row = start_row + size ;
     int index_col = start_col + size ;
     
-    //cout << index_row << ' ' << index_col << ' ' << size << endl;
     for(int i = start_col; i < index_col; i++){
-        //cout << index_row << ' ' << i << "--" << endl;
         if(board[index_row][i] == 0)
             return false;
     }
     
     for(int i = start_row; i < index_row; i++){
         
-        //cout << i << ' ' << index_col << "--" << endl;
         if(board[i][index_col] == 0)
             return false;
     }
@@ -25,12 +22,9 @@ bool check_square(vector<vector<int>> board, int start_row, int start_col, int s
     if(board[index_row][index_col] == 0)
         return false;
     
-    //cout << "---------------------------" << endl;
-    
     return true;
 }
 
-//백트레킹 해야할듯
 int solution(vector<vector<int>> board)
 {
     int answer = 0;
@@ -45,7 +39,6 @@ int solution(vector<vector<int>> board)
                 int len_size = min(row_size - i, col_size - j);
                 
                 for(int len = 1; len < len_size; len++){
-                    //cout << len << " : " << endl;
                     if(check_square(board, i, j , len)){
                         tmp++;
                     }
