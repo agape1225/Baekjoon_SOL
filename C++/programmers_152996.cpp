65b3a17757ee6b38b91e5d65ba504a67107c3ba8
agape1225 <scg9268@gmail.com>
2023-09-18 21:29:16 +0900

    Update programmers_152996.cpp

--- C++/programmers_152996.cpp
+++ C++/programmers_152996.cpp
@@ -4,31 +4,31 @@
 
 using namespace std;
 
-//broute forceÏù∏Í∞Ä...?
+//broute force¿Œ∞°...?
 long long solution(vector<int> weights) {
     long long answer = 0;
-    int cache[4005] = {0};
-    int count_cache[1005] = {0};
-    bool dir_cache[1005] = {0};
-    
-    for(auto it : weights){
-        count_cache[it]++; 
+    int cache[4005] = { 0 };
+    int count_cache[1005] = { 0 };
+    bool dir_cache[1005] = { 0 };
+
+    for (auto it : weights) {
+        count_cache[it]++;
         cache[it * 2]++;
         cache[it * 3]++;
         cache[it * 4]++;
     }
-    
-    for(auto it : weights){
-        
+
+    for (auto it : weights) {
+
         answer += count_cache[it] - 1;
-        
-        if(cache[it * 2] > 1)
+
+        if (cache[it * 2] > 1)
             answer += cache[it * 2] - (count_cache[it]);
-        if(cache[it * 3] > 1)
+        if (cache[it * 3] > 1)
             answer += cache[it * 3] - (count_cache[it]);
-        if(cache[it * 4] > 1)
+        if (cache[it * 4] > 1)
             answer += cache[it * 4] - (count_cache[it]);
     }
-    
+
     return answer / 2;
 }
