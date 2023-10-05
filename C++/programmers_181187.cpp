407de2c57d774f2debb6eba5c9ad1a90dd4dfd2c
agape1225 <scg9268@gmail.com>
2023-10-05 16:13:17 +0900

    Update programmers_181187.cpp

--- C++/programmers_181187.cpp
+++ C++/programmers_181187.cpp
@@ -1,42 +1,18 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include <iostream>
-
 using namespace std;
 
 long long solution(int r1, int r2) {
     long long answer = 0;
-    long long line_ans = 2 + r2 - r1;
 
-    int start = r2;
-    int end = r1;
+    for (int x = 1; x <= r2; x++)
+    {
+        long long maxY = floor(sqrt(r2 * 1LL * r2 - x * 1LL * x));
+        long long minY = x >= r1 ? 0 : ceil(sqrt(r1 * 1LL * r1 - x * 1LL * x));
 
-    for (int x = 1; x < r2; x++) {
-
-        //check start
-        while (pow(x, 2) + pow(start, 2) > pow(r2, 2) && start > 0) {
-            start--;
-        }
-        //check end
-        //1 + 1 < 4
-        while (pow(x, 2) + pow(end, 2) > pow(r1,2) && end > 0) {
-            end--;
-        }
-
-
-        cout << x << ' ' << start << ' ' << end << endl;
-
-        if (end != 0 && start != 0)
-            answer += start - end;
-        else 
-            answer += start - end - 1;
-
+        answer += (maxY - minY + 1);
     }
 
-    return (answer + line_ans) * 4;
-}
-
-int main(void) {
-    cout << solution(2, 5);
+    return answer * 4;
 }
