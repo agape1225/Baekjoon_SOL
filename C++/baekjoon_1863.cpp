a3094f26b1cbd5e2f17a39ffd93e2e6d8699c51a
agape1225 <scg9268@gmail.com>
2023-12-16 15:15:02 +0900

    Update baekjoon_1863.cpp

--- C++/baekjoon_1863.cpp
+++ C++/baekjoon_1863.cpp
@@ -10,45 +10,19 @@ int main(void) {
 
 	cin >> n;
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i <= n; i++) {
 		int x, y;
 		if (i < n)
 			cin >> x >> y;
-		/*else
-			y = 0;*/
-
-		if (s.empty()) {
-			s.push(make_pair(x, y));
+		else
+			y = 0;
+		while (!s.empty() && s.top().second >= y) {
+			if(s.top().second > y)
+				count++;
+			s.pop();
 		}
-		else {
-
-			if (s.top().second > y) {
-				while (!s.empty() && s.top().second >= y) {
-					count++;
-					s.pop();
-				}
-
-				if (y != 0) {
-					if (s.empty())
-						s.push(make_pair(x, y));
-					else if(s.top().second < y)
-						s.push(make_pair(x, y));
-				}
-				//s.push(make_pair(x, y));
-					
-			}
-			else
-				s.push(make_pair(x, y));
-		}
+		s.push(make_pair(x, y));
 	}
-
-	//cout << count;
-
-	while (!s.empty()) {
-		s.pop();
-		count++;
-	}
-
 	cout << count;
 
 	return 0;
