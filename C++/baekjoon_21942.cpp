1314d61d82f2a5b0186e0149efd6cb4d99d5b8a8
agape1225 <scg9268@gmail.com>
2024-01-02 15:45:41 +0900

    Update baekjoon_21942.cpp

--- C++/baekjoon_21942.cpp
+++ C++/baekjoon_21942.cpp
@@ -6,20 +6,34 @@
 
 using namespace std;
 
-int get_diff(string start, string end) {
+int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	int mon = (stoi(end.substr(5, 2)) - stoi(start.substr(5, 2))) * 60 * 24 * 30;
-	int day = (stoi(end.substr(8, 2)) - stoi(start.substr(8, 2))) * 60 * 24;
-	int hour = (stoi(end.substr(11, 2)) - stoi(start.substr(11, 2))) * 60;
-	int min = (stoi(end.substr(14, 2)) - stoi(start.substr(14, 2)));
-	cout << mon << ' ' << day << ' ' << hour << ' ' << min << endl;
-	return mon + day + hour + min;
+long long get_diff(string start, string end) {
+
+	int end_mon = stoi(end.substr(5, 2));
+	int end_mon_tmp = 0;
+	int start_mon = stoi(start.substr(5, 2));
+	int start_mon_tmp = 0;
+
+	for (int i = 0; i < end_mon -1; i++) {
+		end_mon_tmp += days[i];
+	}
+	for (int i = 0; i < start_mon-1; i++) {
+		start_mon_tmp += days[i];
+	}
+
+	long long day = ((stoi(end.substr(8, 2)) + end_mon_tmp) - (stoi(start.substr(8, 2)) + start_mon_tmp)) * 60 * 24;
+	long long hour = (stoi(end.substr(11, 2)) - stoi(start.substr(11, 2))) * 60;
+	long long min = (stoi(end.substr(14, 2)) - stoi(start.substr(14, 2)));
+	//cout << mon << ' ' << day << ' ' << hour << ' ' << min << endl;
+	return day + hour + min;
 }
 
 int main(void) {
-	int N, F, rental_period;
+	int N, F;
+	long long rental_period;
 	string L;
-	map<string, int> ans;
+	map<string, long long> ans;
 	map<pair<string, string>, string> m;
 
 	cin >> N >> L >> F;
@@ -29,21 +43,17 @@ int main(void) {
 		(stoi(L.substr(4,2)) * 60) + 
 		stoi(L.substr(7, 2));
 
-	cout << rental_period << endl;
-
 	for (int i = 0; i < N; i++) {
 		string date, time, P, M;
 		cin >> date >> time >> P >> M;
 		auto it = m.find(make_pair(P, M));
 		if (it != m.end()) {
-			//대여시각
 			string start = (*it).second;
-			//반납시각
 			string end = date + ' ' + time;
 
-			int diff = get_diff(start, end);
+			long long diff = get_diff(start, end);
 			if (rental_period < diff) {
-				int final_fin = (diff - rental_period) * F;
+				long long final_fin = (diff - rental_period) * F;
 				auto is_name_in = ans.find(M);
 				if (is_name_in != ans.end()) {
 					ans[M] += final_fin;
@@ -57,11 +67,14 @@ int main(void) {
 			m.insert(make_pair(make_pair(P, M), date + ' ' + time));
 		}
 	}
-
-	for (auto it : ans) {
-		cout << it.first << ' ' << it.second << '\n';
+	if (ans.size() == 0)
+		cout << -1;
+	else {
+		for (auto it : ans)
+			cout << it.first << ' ' << it.second << '\n';
+		
 	}
-
+	
 	return 0;
 
 }
