ad9b61490a0266b37d36044324d2915ecd820604
agape1225 <scg9268@gmail.com>
2024-01-16 23:05:51 +0900

    Update baekjoon_19583.cpp

--- C++/baekjoon_19583.cpp
+++ C++/baekjoon_19583.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
-#include <map>
-#include <set>
+#include<set>
+#include<map>
+#include<string>
 using namespace std;
+string s, e, q;
+int STT(string s) {
+	int h = stoi(s.substr(0, 2));
+	int m = stoi(s.substr(3, 2));
+	int time = h * 100 + m;
+	return time;
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
 
-int main(void) {
-	int count = 0;
-	map<string, string> m;
-	set<string> s;
-	string S, E, Q;
-	string start_time, name;
+	cin >> s >> e >> q;
 
-	cin >> S >> E >> Q;
+	int s_time = STT(s);
+	int e_time = STT(e);
+	int q_time = STT(q);
 
-	while (cin >> start_time >> name) {
-		if (start_time <= S) {
-			auto it = m.find(name);
-			if (it == m.end()) {
-				m.insert(start_time, name);
-			}
+	set<string> us;
+	set<string> uscnt;
+
+	while (1) {
+		string s1, s2;
+		cin >> s1 >> s2;
+
+		if (s1 == "" && s2 == "") break;
+
+		int s1_time = STT(s1);
+
+		if (s1_time <= s_time) {
+			us.insert(s2);
 		}
-		if (E <= start_time && start_time <= Q) {
-			if (s.find(name) != s.end()) {
-				auto it = m.find(name);
-
-				if (it != m.end()) {
-					count = count + 1;
-					s.insert(name);
-				}
-			}
+		else if (s1_time >= e_time && s1_time <= q_time) {
+			if (us.find(s2) != us.end())
+				uscnt.insert(s2);
 		}
 	}
-	cout << count;
-
-	return 0;
+	cout << uscnt.size() << '\n';
 }
