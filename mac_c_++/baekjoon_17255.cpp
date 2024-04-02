c8ce29d7753ecd1f1f5920fe4e7453fec20eef13
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-04-02 20:55:02 +0900

    2024.04.02

--- mac_c_++/baekjoon_17255.cpp
+++ mac_c_++/baekjoon_17255.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <string>
-#include <set>
+// #include <iostream>
+// #include <string>
+// #include <set>
 
-using namespace std;
+// using namespace std;
 
-string N;
-set<string> dict;
+// string N;
+// set<string> dict;
 
-int answer = 0;
+// int answer = 0;
 
-void get_count(string str, int start, int end, string history){
+// void get_count(string str, int start, int end, string history){
 
-    if(str == N && dict.find(history) == dict.end()){
-        answer++;
-        dict.insert(history);
-    }
+//     if(str == N && dict.find(history) == dict.end()){
+//         answer++;
+//         dict.insert(history);
+//     }
 
-    if(start > 0)
-        get_count(N[start - 1] + str, start - 1, end, history + str);
+//     if(start > 0)
+//         get_count(N[start - 1] + str, start - 1, end, history + str);
 
-    if(end < N.size() - 1)
-        get_count(str + N[end + 1], start, end + 1, history + str);
-}
+//     if(end < N.size() - 1)
+//         get_count(str + N[end + 1], start, end + 1, history + str);
+// }
 
-int main(void){
+// int main(void){
     
-    cin >> N;
+//     cin >> N;
 
-    for(int i = 0; i < N.size(); i++){
-        string tmp(1, N[i]);
-        get_count(tmp, i, i, tmp);
-    }
+//     for(int i = 0; i < N.size(); i++){
+//         string tmp(1, N[i]);
+//         get_count(tmp, i, i, tmp);
+//     }
 
-    cout << answer;
-}
+//     cout << answer;
+// }
