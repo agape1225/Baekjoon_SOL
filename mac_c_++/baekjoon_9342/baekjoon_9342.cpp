0ef49003a973235bbd49285b307c03c5ca1cc61c
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-04-15 22:38:23 +0900

    2024.04.15

--- mac_c_++/baekjoon_9342/baekjoon_9342.cpp
+++ mac_c_++/baekjoon_9342/baekjoon_9342.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
 #include <regex>
+#include <vector>
 
 using namespace std;
+
+bool isMatched(string str){
+    int index = 1;
+    string str_list = "ABCDEF";
+    string str_order = "AFC";
+    int count_a = 0;
+    int count_f = 0;
+    int count_c = 0;
+
+    // if(str_list.find(str[index]) != string::npos){
+    //     index++;
+    // }
+
+    if(str[0] == 'A')
+        count_a++;
+
+    for(int i = 1; i < str.size(); i++){
+
+        if(i == str.size() - 1){
+            if(count_c == 0){
+                if(str[i] == 'C'){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+            else{
+                return str_list.find(str[i]) != string::npos;
+            }
+        }else{
+
+        if(str[i] == 'A'){
+            count_a++;
+        }else if(str[i] == 'F'){
+            if(count_a == 0){
+                return false;
+            }else{
+                count_f++;
+            }
+        }else if(str[i] == 'C'){
+            if(count_a == 0 || count_f == 0){
+                return false;
+            }else{
+                count_c++;
+            }
+        }else
+            return false;
+
+        }
+        
+    }
+}
 
 int main(void){
     int T;
@@ -14,7 +67,7 @@ int main(void){
         string str;
         cin >> str;
 
-        if(regex_match(str, e)){
+        if(isMatched(str)){
             cout << "Infected!" << endl;
         }else{
             cout << "Good" << endl;
