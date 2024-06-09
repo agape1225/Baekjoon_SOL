da53f66b3bbb203d195026b14435cc67527c2993
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-06-09 16:04:33 +0900

    2024.06.09

--- mac_c_++/baekjoon_13022/baekjoon_13022.cpp
+++ mac_c_++/baekjoon_13022/baekjoon_13022.cpp
@@ -1,74 +1,77 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-vector<int> order = {'w', 'o', 'l', 'f'};
+vector<string> get_words(string s){
+    vector<string> words;
+    string tmp = "";
+    bool is_begin = true;
 
-int get_N(int start_num, string s){
-    int count = 0;
-    for(int i = start_num; i < s.size(); i++){
-        if(s[i] == 'w'){
-            return count;
-        }else{
-            
-            count++;
-        }
-    }
-    return -1;
-}
-
-int get_end(int start, int N, string s){
-    for(int i = start + N; i < s.size(); i++){
-        if(i == 'w')
-            return i - 1;
-    }
-    return s.size();
-}
-
-bool is_wolf_num(int start, int end, string s, int N){
-    int index = 0;
-    int tmp_end = start + N;
-    for(int i = start; i < end; i++){
-        if(i == tmp_end){
-            index++;
-            tmp_end = tmp_end + N;
-        }
-
-        // cout << i << ' ' << index << endl;
-
-        if(s[i] != order[index]){
-            return false;
-        }
-    }
-    return true;
-}
-
-int is_ans(string s){
-    int start_num = 0;
     for(int i = 0; i < s.size(); i++){
-        int N = get_N(i, s);
-        // cout << N << endl;
-        int start = i;
-        int end = get_end(start, N, s);
-
-        cout << start << ' ' << end << endl;
-
-        if(!is_wolf_num(start, end, s, N)){
-            return 0;
+        if(s[i] == 'w'){
+            if(is_begin){
+                tmp += s[i];
+            }else{
+                words.push_back(tmp);
+                tmp = "" ;
+                tmp += s[i];
+                is_begin = true;
+            }
+        }else{
+            if(is_begin){
+                is_begin = false;
+            }
+            tmp += s[i];
         }
-
-        i = i + end;
     }
-    return 1;
+    words.push_back(tmp);
+    return words;
+}
+
+int get_N(string s){
+    int count = 0;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == 'w'){
+            count++;
+        }else
+            return count;
+    }
+    return count;
+}
+
+string get_comp_base_str(int N){
+    vector<string> str_order = {"w", "o", "l", "f"};
+    string comp_str = "";
+
+    for(auto it : str_order){
+        string tmp = "";
+        for(int i = 0; i < N; i++){
+            tmp += it;
+        }
+        comp_str += tmp;
+    }
+    return comp_str;
 }
 
 int main(void){
     string s;
-    int ans;
     cin >> s;
 
-    ans = is_ans(s);
-    cout << ans;
-    return ans;
+    vector<string> words = get_words(s);
+
+    for(auto it : words){
+        // cout << it << endl;
+        int N = get_N(it);
+        string comp_base_str = get_comp_base_str(N);
+
+        if(it != comp_base_str){
+            cout << 0;
+            return 0;
+        }
+    }
+
+    cout << 1;
+    return 0;
 }
