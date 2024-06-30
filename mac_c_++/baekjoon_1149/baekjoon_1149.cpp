1269e849d4c755910c02d2491bf117e6d2726e2a
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-06-30 17:02:34 +0900

    1149

---
+++ mac_c_++/baekjoon_1149/baekjoon_1149.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int cost[3][105];
+    int cache[3][105];
+    int N;
+
+    cin >> N;
+
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> cost[i][j];
+        }
+    }
+
+    cache[0][0] = cost[0][0];
+    cache[1][0] = cost[1][0];
+    cache[2][0] = cost[2][0];
+
+    for (int i = 0; i < N; i++)
+    {
+        cache[0][i] = cost[0][i] + min(cache[1][i - 1], cache[2][i - 1]);
+        cache[1][i] = cost[1][i] + min(cache[2][i - 1], cache[0][i - 1]);
+        cache[2][i] = cost[2][i] + min(cache[0][i - 1], cache[1][i - 1]);
+    }
+
+    cout << min(cache[0][N - 1], min(cache[1][N - 1], cache[2][N - 1]));
+
+    return 0;
+}
