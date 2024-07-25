9aadece57cda81fc0da1d69e22f6d5834ea715ca
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-07-25 23:03:26 +0900

    2024.07.25

--- mac_c_++/baekjoon_2579/baekjoon_2579.cpp
+++ mac_c_++/baekjoon_2579/baekjoon_2579.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
 int main(void){
-    vector<int> stairs;
-    vector<int> cache;
-    int N, s;
+
+    int N;
+    int cache[305] = {0};
+    int stairs[305] = {0};
+
     cin >> N;
 
     for(int i = 0; i < N; i++){
-        // int stare;
-        cin >> s;
-        stairs.push_back(s);
-        cache.push_back(s);
+        cin >> stairs[i];
     }
 
+    cache[0] = stairs[0];
+    
     for(int i = 1; i < N; i++){
         if(i == 1){
-            cache[i] = stairs[i] + cache[i - 1];
+            cache[i] = stairs[i] + stairs[i - 1];
         }
-        else if(i == 2){
-            cache[i] = max(stairs[i - 1], cache[i - 2]) + stairs[i];
-        }else{
-            cache[i] = max(cache[i - 3] + stairs[i - 1], cache[i - 2]) + stairs[i];
-        }
+        else if(i == 2)
+            cache[i] = max(stairs[i - 1], stairs[i - 2]) + stairs[i];
+        else
+            cache[i] = max(cache[i - 2], stairs[i - 1] + cache[i - 3]) + stairs[i];
     }
 
     cout << cache[N - 1];
