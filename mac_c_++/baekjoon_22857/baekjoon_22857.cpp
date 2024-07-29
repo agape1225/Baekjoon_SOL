d6c9f29101537bbd625e98faa3b1c7479edb51a5
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-07-29 23:24:35 +0900

    2024.07.29

--- mac_c_++/baekjoon_22857/baekjoon_22857.cpp
+++ mac_c_++/baekjoon_22857/baekjoon_22857.cpp
@@ -15,20 +15,6 @@ int main(void){
         cin >> S[i];
     }
 
-    // for(int i = 0; i <= K; i++){
-    //     for(int j = 1; j <= N; j++){
-    //         bool is_even = S[j - 1] % 2 == 0;
-    //         if(is_even){
-    //             cache[i][j] = cache[i - 1][j] + 1;
-    //         }
-            
-    //         if(!is_even){
-    //             cache[i][j] = cache[i - 1][j - 1];
-    //         }
-            
-    //     }
-    // }
-
     for(int i = 1; i <= N; i++){
         bool is_even = S[i - 1] % 2 == 0;
         for(int j = 0; j <= K; j++){
@@ -45,25 +31,11 @@ int main(void){
 
     for(int i = 0; i <= N; i++){
         for(int j = 0; j <= K; j++){
-            // cout << cache[i][j] << ' ';
                 ans = max(ans, cache[i][j]);   
         }
-        // cout << endl;
     }
-
-    // for(int i = 0; i <= K; i++){
-    //     for(int j = 1; j <= N; j++){
-    //         // cout << cache[i][j] << ' ';
-    //         ans = max(ans, cache[i][j]);   
-            
-    //     }
-    //     // cout << endl;
-    // }
     
     cout << ans;
 
-
     return 0;
-
-
 }
