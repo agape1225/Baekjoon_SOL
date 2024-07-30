7bde73d454c2d90ba9e62a516a711d93699479ed
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-07-30 21:35:15 +0900

    2024.07.30

--- mac_c_++/baekjoon_22857/baekjoon_22857.cpp
+++ mac_c_++/baekjoon_22857/baekjoon_22857.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-int S[50005];
-int cache[50005][105] = {0};
+    int cache[50001][101] = {0};
+    int arr[50001];
 
 int main(void){
+
+
     int N, K;
-    
     int ans = -1;
+
     cin >> N >> K;
 
     for(int i = 0; i < N; i++){
-        cin >> S[i];
+        cin >> arr[i];
     }
 
     for(int i = 1; i <= N; i++){
-        bool is_even = S[i - 1] % 2 == 0;
-        for(int j = 0; j <= K; j++){
+        bool is_even = arr[i] % 2 == 0;
+        for(int j = 1; j <= K; j++){
 
             if(is_even){
                 cache[i][j] = cache[i - 1][j] + 1;
-            }
-            
-            if(j != 0 && !is_even){
+            }else {
                 cache[i][j] = cache[i - 1][j - 1];
             }
+            
         }
     }
 
     for(int i = 0; i <= N; i++){
         for(int j = 0; j <= K; j++){
-                ans = max(ans, cache[i][j]);   
+            ans = max(ans, cache[i][j]);
         }
     }
-    
+
     cout << ans;
 
     return 0;
