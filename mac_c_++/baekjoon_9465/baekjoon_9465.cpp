d0004c53c97b97c464a3c0e925c17cf2a79b52fd
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-01 22:57:39 +0900

    2024.08.01

--- mac_c_++/baekjoon_9465/baekjoon_9465.cpp
+++ mac_c_++/baekjoon_9465/baekjoon_9465.cpp
@@ -4,46 +4,55 @@
 using namespace std;
 
 int main(void){
+
+    
+
     int T;
     cin >> T;
 
     for(int t = 0; t < T; t++){
-        int sticker[2][100005];
-        int cache[2][100005];
+        int cache[2][100005] = {0};
+        int sticker[2][100005] = {0};
+        int N;
 
-        int n;
-
-        cin >> n;
+        cin >> N;
 
         for(int i = 0; i < 2; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < N; j++){
                 cin >> sticker[i][j];
             }
         }
 
         cache[0][0] = sticker[0][0];
-        cache[0][1] = sticker[0][1];
+        cache[1][0] = sticker[1][0];
 
-		if (n == 1) {
-			cout << max(cache[0][0], cache[1][0]) << '\n';
-			continue;
-		}
+        if(N == 1){
+            cout << max(cache[0][0], cache[1][0]) << endl;
+            continue;
+        }
 
-		cache[0][1] = sticker[0][1] + sticker[1][0];
+        cache[0][1] = sticker[0][1] + sticker[1][0];
 		cache[1][1] = sticker[1][1] + sticker[0][0];
 
-		if (n == 2) {
+		if (N == 2) {
 			cout << max(cache[0][1], cache[1][1]) << '\n';
 			continue;
 		}
+        
+        for(int i = 2; i < N; i++){
 
-        for(int i = 1; i < n; i++){
+            //차이점 찾기
+            cache[0][i] = max(cache[0][i - 2] + sticker[1][i - 1], cache[1][i - 2]) + sticker[0][i];
+            cache[1][i] = max(cache[1][i - 2] + sticker[0][i - 1], cache[0][i - 2]) + sticker[1][i];
+
             cache[0][i] = max(cache[1][i - 1] + sticker[0][i], cache[0][i - 1]);
             cache[1][i] = max(cache[0][i - 1] + sticker[1][i], cache[1][i - 1]);
         }
 
-        cout << max(cache[0][n - 1], cache[1][n - 1]) << '\n';
+        cout << max(cache[0][N - 1], cache[1][N - 1]) << '\n';
     }
 
     return 0;
+
+
 }
