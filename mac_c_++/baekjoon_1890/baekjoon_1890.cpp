e3eb3dfc5cd7e5ba09c59ba34d586f97c70f1ea1
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-02 22:04:41 +0900

    2024.08.02

--- mac_c_++/baekjoon_1890/baekjoon_1890.cpp
+++ mac_c_++/baekjoon_1890/baekjoon_1890.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main(void){
-    //...?
-
-    int bord[105][105] = {0};
+    int N;
+    int bord[105][105];
     long long cache[105][105] = {0};
 
-    int N;
     cin >> N;
 
     for(int i = 0; i < N; i++){
@@ -17,25 +16,47 @@ int main(void){
         }
     }
 
-    if(N > bord[0][0]){
-        cache[0][bord[0][0]] = 1;
-        cache[bord[0][0]][0] = 1;
-    }
+    cache[0][0] = 1;    
 
     for(int i = 0; i < N; i++){
+
         for(int j = 0; j < N; j++){
+            if(cache[i][j] != 0){
 
-            if(cache[i][j] > 0 && bord[i][j] > 0){
-                int num = bord[i][j];
-                if(i + num < N){
-                    cache[i + num][j]+= cache[i][j];
-                }
+                if(i == N - 1 && j == N - 1)
+                continue;
+            int new_i = i + bord[i][j];
+            int new_j = j + bord[i][j];
+            
 
-                if(j + num < N){
-                    cache[i][j + num] += cache[i][j];
-                }
+            if(new_i < N){
+                // cout << i << ' ' << j << ' ' << new_i << ' ' << j << endl << endl;
+                cache[new_i][j] += cache[i][j];
             }
+
+            if(new_j < N){
+                // cout << i << ' ' << j << ' ' << i << ' ' << new_j << endl << endl;
+                cache[i][new_j] += cache[i][j];
+            }
+
+            }
+
+            
+
+            // cout << endl; 
+            // for(int k = 0; k < N; k++){
+            //     for(int l = 0; l < N; l++){
+            //         cout << cache[k][l] << ' ';
+            //     }
+            // cout << endl;
+            // }
         }
+            
+        // }
+
+        
+        
+
     }
 
     cout << cache[N - 1][N - 1];
