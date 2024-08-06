513ee39188129267fcd011d71468f460326a17c2
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-06 22:27:54 +0900

    2024.08.06

--- mac_c_++/baekjoon_1106/baekjoon_1106.cpp
+++ mac_c_++/baekjoon_1106/baekjoon_1106.cpp
@@ -1,31 +1,37 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#define MAX 100*1000
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
 
 int cost[21], customer[21];
-int dp[MAX + 1];
+int cache[100001] = {0};
 
 int max(int x, int y) {
 	return x > y ? x : y;
 }
 
 int main() {
-	int c, n;
+	int C, N;
 	
-    	// 입력
-	scanf("%d%d", &c, &n);
-	for (int i = 1; i <= n; i++)
-		scanf("%d%d", &cost[i], &customer[i]);
+	cin >> C >> N;
+
+	for (int i = 1; i <= N; i++)
+		cin >> cost[i] >> customer[i];
 
 	// dp 배열 구하기
-	for (int i = 1; i <= n; i++)
-		for (int j = cost[i]; j <= MAX; j++)
-			dp[j] = max(dp[j], dp[j - cost[i]] + customer[i]);
+	for (int i = 1; i <= N; i++){
+		for (int j = cost[i]; j <= 100000; j++){
+			cache[j] = max(cache[j], cache[j - cost[i]] + customer[i]);
+		}
+			
+	}
+		
 
 	// 출력
-	for (int i = 1; i <= MAX; i++)
-		if (dp[i] >= c) {
-			printf("%d", i);
+	for (int i = 1; i <= 100000; i++)
+		if (cache[i] >= C) {
+			cout << i;
 			break;
 		}
         
