65b7c9535be9dbd7eacc7d17172921f762a306c4
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-08 21:04:53 +0900

    2024.08.08

--- mac_c_++/baekjoon_9095/baekjoon_9095.cpp
+++ mac_c_++/baekjoon_9095/baekjoon_9095.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 int main(void){
+
     int T;
     cin >> T;
+
     for(int t = 0; t < T; t++){
+        int cache[11] = {0};
+    int N;
+    
+    cin >> N;
 
-        int N;
-        int cache[11];
-        cin >> N;
+    cache[1] = 1;
+    cache[2] = 2;
+    cache[3] = 4;
 
-        //이 부분만 풀어야한다.
-        cache[1] = 1;
-        cache[2] = 2;
-        cache[3] = 4;
-
-        for(int i = 4; i <= N; i++){
-            cache[i] = cache[i - 1] + cache[i - 2] + cache[i - 3];
-        }
-
-        cout << cache[N];
+    for(int i = 4; i <= N; i++){
+        cache[i] = cache[i - 1] + cache[i - 2] + cache[i - 3];
     }
+
+    cout << cache[N] << '\n';
+    }
+
+    
 
     return 0;
 }
