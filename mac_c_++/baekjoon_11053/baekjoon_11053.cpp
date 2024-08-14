db8d9d205bb3e8ca02bfbbe267b784fa996ae79f
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-14 21:03:55 +0900

    2024.01.14

--- mac_c_++/baekjoon_11053/baekjoon_11053.cpp
+++ mac_c_++/baekjoon_11053/baekjoon_11053.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 int main(void){
-    int arr[100005];
-    int cache[100005];
+    int arr[1005];
+    int cache[1005] = {0};
     int N;
     int ans = -1;
 
     cin >> N;
+
     for(int i = 0; i < N; i++){
         cin >> arr[i];
+        cache[i] = 1;
     }
 
-    ans = cache[0] = arr[0];
+   
 
-    for(int i = 1; i < N; i++){
-        cache[i] = max(arr[i], cache[i - 1] + arr[i]);
+    for(int i = 0; i < N; i++){
+        
+        for(int j = i + 1; j < N; j++){
+            if(arr[i] < arr[j]){
+                cache[j] = max(cache[j], cache[i] + 1);
+            }
+        }
+
         ans = max(ans, cache[i]);
     }
 
-    cout << ans;
+    cout << cache[N - 1];
 
     return 0;
-
 }
