5f5a7437628d6e868b4ff9ca45bc02de48e1a941
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-25 16:04:55 +0900

    2024.08.25

--- mac_c_++/baekjoon_22857/baekjoon_22857.cpp
+++ mac_c_++/baekjoon_22857/baekjoon_22857.cpp
@@ -3,41 +3,54 @@
 
 using namespace std;
 
-    int cache[50001][101] = {0};
-    int arr[50001];
+int cache[50005][105] = {0};
+int arr[50005] = {0};
 
 int main(void){
-
-
     int N, K;
     int ans = -1;
 
     cin >> N >> K;
 
-    for(int i = 0; i < N; i++){
+    for(int i = 1; i <= N; i++){
         cin >> arr[i];
     }
 
-    for(int i = 1; i <= N; i++){
-        bool is_even = arr[i] % 2 == 0;
-        for(int j = 1; j <= K; j++){
+    // 뒤에서 하나를 삭제
 
-            if(is_even){
-                cache[i][j] = cache[i - 1][j] + 1;
-            }else {
-                cache[i][j] = cache[i - 1][j - 1];
+    for(int i = 1; i <= N; i++){
+
+        bool is_even = arr[i] % 2 == 0;
+
+        for(int j = 0; j <= K; j++){
+
+            if(j == 0){
+                if(is_even){
+                    cache[i][j] = cache[i - 1][j] + 1;
+                }else{
+                    cache[i][j] = 0;
+                }
+                    
+            }else{
+                if(is_even){
+                    cache[i][j] = cache[i - 1][j] + 1;
+                }else{
+                    cache[i][j] =  cache[i - 1][j - 1];
+                }
             }
-            
         }
     }
 
     for(int i = 0; i <= N; i++){
         for(int j = 0; j <= K; j++){
-            ans = max(ans, cache[i][j]);
+            // cout << cache[i][j] << ' ';
+                ans = max(ans, cache[i][j]);   
         }
+        // cout << endl;
     }
 
     cout << ans;
 
     return 0;
+
 }
