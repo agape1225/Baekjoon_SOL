2fd98f62ef32c2797f7821ab7f6f79cc0c0cc88c
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-28 21:45:51 +0900

    2024.08.28

--- mac_c_++/baekjoon_12865/baekjoon_12865.cpp
+++ mac_c_++/baekjoon_12865/baekjoon_12865.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
-#include <utility>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<pair<int, int>> stuff;
-int cache[101][100001] = { 0 };
+int N, K;
+	int W[105];
+	int V[105];
+	int cache[105][100001];
 
-
-int main(void) {
-
-	int N, K;
-	int W, V;
+int main(void){
+	
 	cin >> N >> K;
-	for (int i = 0; i < N; i++) {
-		cin >> W >> V;
-		stuff.push_back(make_pair(W, V));
+
+	for(int i = 0; i < N; i++){
+		cin >> W[i] >> V[i];
 	}
 
 	for(int i = 1; i <= N; i++){
-		W = stuff[i - 1].first;
-		V = stuff[i - 1].second;
-
 		for(int j = 1; j <= K; j++){
-			if(j - W >= 0){
-				cache[i][j] = max(cache[i - 1][j], cache[i - 1][j - W] + V);
+			if(j - W[i - 1] >= 0){
+				cache[i][j] = max(cache[i - 1][j], cache[i - 1][j - W[i - 1]] + V[i - 1]);
 			}else{
 				cache[i][j] = cache[i - 1][j];
 			}
 		}
 	}
-	
+
 	cout << cache[N][K];
 
-
 	return 0;
+
 }
