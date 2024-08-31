d423ba5703121e86baec346671bf47a035fb32e4
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-08-31 14:38:31 +0900

    Update baekjoon_9095.cpp

--- mac_c_++/baekjoon_9095/baekjoon_9095.cpp
+++ mac_c_++/baekjoon_9095/baekjoon_9095.cpp
@@ -4,28 +4,37 @@
 using namespace std;
 
 int main(void){
-
     int T;
     cin >> T;
 
     for(int t = 0; t < T; t++){
+        int n;
         int cache[11] = {0};
-    int N;
-    
-    cin >> N;
 
-    cache[1] = 1;
-    cache[2] = 2;
-    cache[3] = 4;
+        cin >> n;
 
-    for(int i = 4; i <= N; i++){
-        cache[i] = cache[i - 1] + cache[i - 2] + cache[i - 3];
+        cache[1] = 1;
+        cache[2] = 2;
+        cache[3] = 4;
+
+        /*
+            1       +3
+
+            1+1     +2
+            2       +2
+
+            1+1+1   +1
+            1+2     +1
+            2+1     +1
+            3       +1
+        */
+
+       for(int i = 4; i <= n; i++ ){
+            cache[i] = cache[i - 1] + cache[i - 2] + cache[i - 3];
+       }
+
+       cout << cache[n] << '\n';
     }
-
-    cout << cache[N] << '\n';
-    }
-
-    
 
     return 0;
 }
