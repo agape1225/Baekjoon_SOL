d414a40a7329623a2770ce782ee67de779245182
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-09-01 17:11:06 +0900

    2024.09.01

--- mac_c_++/baekjoon_2579/baekjoon_2579.cpp
+++ mac_c_++/baekjoon_2579/baekjoon_2579.cpp
@@ -4,26 +4,26 @@
 using namespace std;
 
 int main(void){
-    int stare[301] = {0};
-    int cache[301] = {0};
+    int stairs[305] = {0};
+    int cache[305] = {0};
     int N;
 
     cin >> N;
 
     for(int i = 0; i < N; i++){
-        cin >> stare[i];
+        cin >> stairs[i];
     }
 
-    cache[0] = stare[0];
-    cache[1] = stare[0] + stare[1];
-    cache[2] = max(stare[0], stare[1]) + stare[2];
+    cache[0] = stairs[0];
+    cache[1] = stairs[1] + stairs[0];
+
+    cache[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
 
     for(int i = 3; i < N; i++){
-        cache[i] = max(cache[i - 3] + stare[i - 1], cache[i - 2] ) + stare[i];
+        cache[i] = max(stairs[i -1] + cache[i - 3], cache[i - 2]) + stairs[i];
     }
 
     cout << cache[N - 1];
 
     return 0;
-
 }
