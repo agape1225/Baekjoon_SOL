0f5bdb05d62080f333a1911be6e592081820e644
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-09-06 22:58:44 +0900

    2024.09.06

--- mac_c_++/baekjoon_11726/baekjoon_11726.cpp
+++ mac_c_++/baekjoon_11726/baekjoon_11726.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 int main(void){
     int cache[1005] = {0};
-    int n;
+    int N;
+    cin >> N;
 
-    cin >> n;
+    cache[1] = 1;
+    cache[2] = 2;
 
-    cache[0] = 1;
-    cache[1] = 2;
-
-    for(int i = 2; i < n; i++){
-        cache[i] = (cache[i - 1] + cache[i - 2]) % 10007;
+    for(int i = 3; i <= N; i++){
+        cache[i] = (cache[i - 1] + cache[i - 2]) % 100007;
     }
-
-    cout << cache[n - 1];
+    
+    cout << cache[N];
 
     return 0;
 }
