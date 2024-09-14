05504d5a322b33dffed8e89206d53b07ec700b41
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-09-14 16:20:46 +0900

    2024.09.14

--- mac_c_++/baekjoon_9465/baekjoon_9465.cpp
+++ mac_c_++/baekjoon_9465/baekjoon_9465.cpp
@@ -4,21 +4,19 @@
 using namespace std;
 
 int main(void){
-
-    
-
     int T;
     cin >> T;
 
     for(int t = 0; t < T; t++){
-        int cache[2][100005] = {0};
-        int sticker[2][100005] = {0};
-        int N;
 
-        cin >> N;
+        int sticker[2][100005] = {0};
+        int cache[2][100005];
+        int n;
+
+        cin >> n;
 
         for(int i = 0; i < 2; i++){
-            for(int j = 0; j < N; j++){
+            for(int j = 0; j < n; j++){
                 cin >> sticker[i][j];
             }
         }
@@ -26,33 +24,16 @@ int main(void){
         cache[0][0] = sticker[0][0];
         cache[1][0] = sticker[1][0];
 
-        if(N == 1){
-            cout << max(cache[0][0], cache[1][0]) << endl;
-            continue;
+        cache[0][1] = sticker[1][0] + sticker[0][1];
+        cache[1][1] = sticker[0][0] + sticker[1][1];
+
+        for(int i = 2; i < n; i++){
+            cache[0][i] = max(cache[1][i - 1] + sticker[0][i], cache[0][ i -  1]);
+            cache[1][i] = max(cache[0][i - 1] + sticker[1][i], cache[1][ i -  1]);
         }
 
-        cache[0][1] = sticker[0][1] + sticker[1][0];
-		cache[1][1] = sticker[1][1] + sticker[0][0];
-
-		if (N == 2) {
-			cout << max(cache[0][1], cache[1][1]) << '\n';
-			continue;
-		}
-        
-        for(int i = 2; i < N; i++){
-
-            //차이점 찾기
-            cache[0][i] = max(cache[0][i - 2] + sticker[1][i - 1], cache[1][i - 2]) + sticker[0][i];
-            cache[1][i] = max(cache[1][i - 2] + sticker[0][i - 1], cache[0][i - 2]) + sticker[1][i];
-
-            cache[0][i] = max(cache[1][i - 1] + sticker[0][i], cache[0][i - 1]);
-            cache[1][i] = max(cache[0][i - 1] + sticker[1][i], cache[1][i - 1]);
-        }
-
-        cout << max(cache[0][N - 1], cache[1][N - 1]) << '\n';
+        cout << max(cache[0][n - 1], cache[1][n - 1]);
     }
 
     return 0;
-
-
 }
