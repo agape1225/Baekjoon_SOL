8d8170fb597f8181ba3d93f495b9297175139552
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-09-21 19:54:40 +0900

    2024.09.21

--- mac_c_++/baekjoon_1890/baekjoon_1890.cpp
+++ mac_c_++/baekjoon_1890/baekjoon_1890.cpp
@@ -4,62 +4,43 @@
 using namespace std;
 
 int main(void){
+
     int N;
-    int bord[105][105];
+    int board[105][105];
     long long cache[105][105] = {0};
 
     cin >> N;
 
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            cin >> bord[i][j];
+            cin >> board[i][j];
         }
     }
 
-    cache[0][0] = 1;    
+    cache[0][0] = 1;
 
     for(int i = 0; i < N; i++){
-
         for(int j = 0; j < N; j++){
-            if(cache[i][j] != 0){
-
-                if(i == N - 1 && j == N - 1)
+            if(i == N - 1 && j == N - 1)
                 continue;
-            int new_i = i + bord[i][j];
-            int new_j = j + bord[i][j];
-            
+            if(cache[i][j] > 0){
 
-            if(new_i < N){
-                // cout << i << ' ' << j << ' ' << new_i << ' ' << j << endl << endl;
-                cache[new_i][j] += cache[i][j];
-            }
+                int dir = board[i][j];
 
-            if(new_j < N){
-                // cout << i << ' ' << j << ' ' << i << ' ' << new_j << endl << endl;
-                cache[i][new_j] += cache[i][j];
-            }
+                if(i + dir < N){
+                    cache[i + dir][j] += cache[i][j];
+                }
+
+                if(j + dir < N){
+                    cache[i][j + dir] += cache[i][j];
+                }
 
             }
-
-            
-
-            // cout << endl; 
-            // for(int k = 0; k < N; k++){
-            //     for(int l = 0; l < N; l++){
-            //         cout << cache[k][l] << ' ';
-            //     }
-            // cout << endl;
-            // }
         }
-            
-        // }
-
-        
-        
-
     }
 
     cout << cache[N - 1][N - 1];
+
 
     return 0;
 
