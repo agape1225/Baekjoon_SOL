d1c62d48ea7f537e3ec8233015b37fda60973c46
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-09-25 18:29:34 +0900

    Create programmers_340212.cpp

---
+++ mac_c_++/programmers_340212/programmers_340212.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int get_max(vector<int> diffs){
+    int max_level = diffs[0];
+    
+    for(int i = 1; i < diffs.size(); i++){
+        max_level = max(max_level, diffs[i]);
+    }
+    
+    return max_level;
+}
+
+long long get_time(vector<int> diffs, vector<int> times, int level){
+    long long time = 0;
+    
+    for(int i = 0; i < diffs.size(); i++){
+        int diff = diffs[i];
+        int time_cur = times[i];
+        
+        if(level < diff){
+            int time_prev = times[i - 1];
+            time += ((time_cur + time_prev) * (diff - level)) + time_cur;
+        }else{
+            time += time_cur;
+        }
+    }
+    return time;
+}
+
+int solution(vector<int> diffs, vector<int> times, long long limit) {
+    int answer = 0;
+    int min = 1;
+    int max = get_max(diffs);
+    int mid;
+    
+    while(min <= max){
+        mid = (max + min) / 2;
+        long long time = get_time(diffs, times, mid);
+        if(time > limit){
+            min = mid + 1;
+        }else{
+            answer = mid;
+            max = mid - 1;
+        }
+        
+    }
+    
+    // answer = mid;
+    
+    return answer;
+}
