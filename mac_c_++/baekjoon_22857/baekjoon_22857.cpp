39a3eca1435bb2071689cf61416ead6f4b1e82e4
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-10-06 17:10:07 +0900

    2024.10.06

--- mac_c_++/baekjoon_22857/baekjoon_22857.cpp
+++ mac_c_++/baekjoon_22857/baekjoon_22857.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-    int cache[50001][101] = {0};
-    int arr[50001];
-
-int main(void){
-
+int main() {
 
     int N, K;
+    int arr[50005] = {0};
+    int cache[50005][105] = {0};
     int ans = -1;
-
+    
     cin >> N >> K;
 
     for(int i = 0; i < N; i++){
-        cin >> arr[i];
+        cin >> arr[i + 1];
     }
 
     for(int i = 1; i <= N; i++){
-        bool is_even = arr[i - 1] % 2 == 0;
         for(int j = 0; j <= K; j++){
-
-            if(is_even){
+            if(arr[i] % 2 == 0){
                 cache[i][j] = cache[i - 1][j] + 1;
-            }else if(j != -1){
+            }else{
                 cache[i][j] = cache[i - 1][j - 1];
             }
-            
         }
     }
 
@@ -37,14 +31,8 @@ int main(void){
         }
     }
 
-    for(int i = 0; i <= N; i++){
-        for(int j = 0; j <= K; j++){
-            cout << cache[i][j] << ' ';
-        }
-        cout << endl;
-    }
-
     cout << ans;
-
+    
+    
     return 0;
 }
--- mac_c_++/baekjoon_9465/baekjoon_9465.cpp
+++ mac_c_++/baekjoon_9465/baekjoon_9465.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main(void){
+
     int T;
     cin >> T;
-
     for(int t = 0; t < T; t++){
 
-        int sticker[2][100005] = {0};
-        int cache[2][100005];
         int n;
+        int cache[2][100005] = {0};
+        int sticker[2][100005] = {0};
 
         cin >> n;
 
@@ -24,16 +24,18 @@ int main(void){
         cache[0][0] = sticker[0][0];
         cache[1][0] = sticker[1][0];
 
-        cache[0][1] = sticker[1][0] + sticker[0][1];
-        cache[1][1] = sticker[0][0] + sticker[1][1];
+        // cache[0][1] = max(cache[0][0], sticker[1][0] + sticker[0][1]);
+        // cache[1][1] = max(cache[1][0], sticker[0][0] + sticker[1][1]);
 
-        for(int i = 2; i < n; i++){
-            cache[0][i] = max(cache[1][i - 1] + sticker[0][i], cache[0][ i -  1]);
-            cache[1][i] = max(cache[0][i - 1] + sticker[1][i], cache[1][ i -  1]);
+        for(int i = 1; i < n; i++){
+            cache[0][i] = max(cache[0][i - 1], sticker[0][i] + cache[1][i - 1]);
+            cache[1][i] = max(cache[1][i - 1], sticker[1][i] + cache[0][i - 1]);
         }
 
-        cout << max(cache[0][n - 1], cache[1][n - 1]);
+        cout << max(cache[0][n - 1], cache[1][n - 1]) << '\n';
+
     }
 
     return 0;
+
 }
