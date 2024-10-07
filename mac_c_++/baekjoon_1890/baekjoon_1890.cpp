6811c82d25fbc7d493ded876a72e7658e2d9dcc6
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-10-07 14:00:26 +0900

    Update baekjoon_1890.cpp

--- mac_c_++/baekjoon_1890/baekjoon_1890.cpp
+++ mac_c_++/baekjoon_1890/baekjoon_1890.cpp
@@ -3,12 +3,11 @@
 
 using namespace std;
 
-int main(void){
-
+int main() {
     int N;
     int board[105][105];
     long long cache[105][105] = {0};
-
+    
     cin >> N;
 
     for(int i = 0; i < N; i++){
@@ -18,30 +17,30 @@ int main(void){
     }
 
     cache[0][0] = 1;
-
+    
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
+            
             if(i == N - 1 && j == N - 1)
                 continue;
-            if(cache[i][j] > 0){
+            
+            long long tmp = cache[i][j];
 
-                int dir = board[i][j];
-
-                if(i + dir < N){
-                    cache[i + dir][j] += cache[i][j];
+            
+            
+            if( tmp > 0){
+                int dis = board[i][j];
+                if(i + dis < N){
+                    cache[i + dis][j] += cache[i][j];
                 }
-
-                if(j + dir < N){
-                    cache[i][j + dir] += cache[i][j];
+                if(j + dis < N){
+                    cache[i][j + dis] += cache[i][j];
                 }
-
             }
         }
     }
 
     cout << cache[N - 1][N - 1];
-
-
+    
     return 0;
-
 }
