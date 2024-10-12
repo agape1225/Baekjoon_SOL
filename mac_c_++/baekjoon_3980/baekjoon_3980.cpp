54c5f531c5231248b1551cb910958f967d677090
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-10-12 15:53:56 +0900

    10,12

---
+++ mac_c_++/baekjoon_3980/baekjoon_3980.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    int T;
+    cin >> T;
+
+    for(int t = 0; t < T; t++){
+        int ability[11][11] = {0};
+        int playerIndex[11] = {0}; 
+        int answer = 0;
+        for(int i = 0; i < 11; i++){
+            for(int j = 0; j < 11; j++){
+                cin >> ability[i][j];
+            }
+        }
+
+        for(int j = 0; j < 11; j++){
+            int tmp = -1;
+
+            for(int i = 0; i < 11; i++){
+                tmp = max(tmp, ability[i][j]);
+            }
+
+            answer += tmp;
+        }
+        cout << answer << '\n';
+    }
+
+    return 0;
+
+}
