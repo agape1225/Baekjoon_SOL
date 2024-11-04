c280d7ecdbb2ffb98f4efaf28fcc63e9880b246d
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-11-04 22:37:26 +0900

    update 11.04

--- mac_c_++/baekjoon_16168/baekjoon_16168.cpp
+++ mac_c_++/baekjoon_16168/baekjoon_16168.cpp
@@ -15,35 +15,26 @@ using namespace std;
 
 void dfs(int current_node, int count){
 
-    if(count == E){
+    if(count >= E){
         bool flag = true;
         for(int i = 1; i <= V; i++){
             if(!visited_n[i]){
                 flag = false;
             }
-                
         }
-
-        if(flag){
+        if(flag)
             is_ans = true;
+        return;
+    }
+
+    for(auto it : graph[current_node]){
+        if(!visited_e[current_node][it]){
+            visited_e[current_node][it] = true;
+            visited_e[it][current_node] = true;
+            visited_n[it] = true;
+            dfs(it, count + 1);
+            }
         }
-    }
-
-    // bool ans = false;
-
-    for(int i = 0; i < graph[current_node].size(); i++){
-        int next = graph[current_node][i]; // 노드 x의 연결되어 있는 다음 노드
-        graph[current_node].erase(graph[current_node].begin()+i); // 제거
-        
-        // 그 노드에서도 상호 연결이 되어있기 때문에 인덱스를 찾아 연결을 끊어줌
-        auto it = find(graph[next].begin(),graph[next].end(),current_node);
-        graph[next].erase(it);
-        // or 연산을 통해 하나라도 true면 true
-        visited_n[next] = true;
-        dfs(next,count+1);
-        i--; // i가 하나 줄었으므로 i--
-    }
-    // return ans;
 }
 
 int main(void){
@@ -61,15 +52,21 @@ int main(void){
 
 
     for(int i = 1; i <= V; i++){
-        int size = graph[i].size();
-        if(size > max_size){
-            max_size = size;
-            start_node = i;
+        for(int j = 1; j <= V; j++){
+            visited_n[j] = false;
         }
+        for(int j = 1; j <= V; j++){
+            for(int k = 1; k <= V; k++){
+                visited_e[j][k] = false;
+            }
+        }
+        visited_n[i] = true;
+        dfs(i, 0);
     }
 
-    visited_n[start_node] = true;
-    dfs(start_node, 0);
+   
+
+    
 
     if(is_ans){
         cout << "YES";
