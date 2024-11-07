dc9fa0173d7389f9c3e8a8244dd45136ca8825e2
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-11-07 22:41:02 +0900

    2024.11.07

--- mac_c_++/baekjoon_4095/baekjoon_4095.cpp
+++ mac_c_++/baekjoon_4095/baekjoon_4095.cpp
@@ -3,20 +3,6 @@
 
 using namespace std;
 
-int arr[1005][1005] = {0};
-
-bool is_ans(int start_row, int start_col, int end_row, int end_col){
-    for(int row = start_row; row < end_row; row++){
-        for(int col = start_col; col < end_col; col++){
-            if(arr[row][col] == 0){
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
 int main(void){
     
     int N, M;
@@ -25,38 +11,32 @@ int main(void){
 
     while(N != 0 && M != 0){
 
+        int arr[1005][1005] = {0};
+        int dp[1005][1005] = {0};
+
         int ans = 0;
-        int max = 0;
+        // int max = 0;
 
         for(int row = 0; row < N; row++){
             for(int col = 0; col < M; col++){
                 cin >> arr[row][col];
+                dp[row][col] = arr[row][col];
+                ans = max(dp[row][col], ans);
             }
         }
 
-        for(int start_row = 0; start_row < N; start_row++){
-            for(int start_col = 0; start_col < M; start_col++){
-
-                for(int end_row = start_row + 1; end_row <= N; end_row++){
-                    for(int end_col = start_col + 1; end_col <= M; end_col++){
-
-                        if(start_row == end_row || start_col == end_col){
-                            continue;
-                        }
-
-                        if(is_ans(start_row, start_col, end_row, end_col)){
-                            // cout << start_row << ' ' << start_col << ' ' << end_row << ' ' << end_col << endl;
-                            int tmp = (end_row - start_row) * (end_col - start_col);
-                            if(max < tmp){
-                                ans = end_row - start_row;
-                                max = tmp;
-                            }
-                        }
-
-                    }
+        for(int row = 1; row < N; row++){
+            for(int col = 1; col < M; col++){
+                if(arr[row][col] == 1){
+                    dp[row][col] = min( dp[row - 1][col], min(dp[row][col - 1], dp[row - 1][col - 1])) + 1;
                 }
                 
+            }
+        }
 
+        for(int row = 0; row < N; row++){
+            for(int col = 0; col < M; col++){
+                ans = max(ans, dp[row][col]);
             }
         }
 
