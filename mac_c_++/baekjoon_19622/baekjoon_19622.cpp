070a2223154a38283d2c95fca37fc50d706a344c
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-12-02 19:28:05 +0900

    2024.12.02

--- mac_c_++/baekjoon_19622/baekjoon_19622.cpp
+++ mac_c_++/baekjoon_19622/baekjoon_19622.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <utility>
+#include <vector>
 
 using namespace std;
-vector<pair<pair<int, int> ,int>> timeline;
-long long dp[100005][2];
-int N;
 
 int main(void){
-    
+    vector<pair<pair<int, int> , int>> meetings;
+    int cache[100005][2] = {0};
+
+    int N;
     int start, end, people;
-    
 
     cin >> N;
-
     for(int i = 0; i < N; i++){
         cin >> start >> end >> people;
-        timeline.push_back(make_pair(make_pair(start, end), people));
+
+        meetings.push_back(
+            make_pair(
+                make_pair(start, end), people
+            )
+        );
     }
 
-    dp[0][1] = timeline[0].second;
-    
-    dp[1][0] = timeline[0].second;
-    dp[1][1] = timeline[1].second;
+    //0은 선택
+    //1은 선택하지 않음
 
-    for(int i = 2; i < N; i++){
-        dp[i][0] = max(dp[i - 1][1], dp[i - 1][0]);
-        dp[i][1] = dp[i - 1][0] + timeline[i].second;
+    cache[0][1] = meetings[0].second;
+
+    if(N > 1){
+        cache[1][0] = meetings[1].second;
+        cache[1][1] = meetings[0].second;
     }
 
-    cout << max(dp[N - 1][0], dp[N - 1][1]);
+    for(int i  = 2; i < N; i++){
+
+        cache[i][0] = cache[i - 1][1] + meetings[i].second;
+        cache[i][1] = max(cache[i - 1][1], cache[i - 1][0]);
+
+    }
+
+    cout << max(cache[N - 1][0], cache[N - 1][1]);
 
     return 0;
-}   
+}
--- mac_c_++/baekjoon_4095/baekjoon_4095.cpp
+++ mac_c_++/baekjoon_4095/baekjoon_4095.cpp
@@ -4,47 +4,49 @@
 using namespace std;
 
 int main(void){
-    
+
+
+
+
     int N, M;
-    
+    int arr[1005][1005] = {0};
+    int cache[1005][1005] = {0};
+
     cin >> N >> M;
 
     while(N != 0 && M != 0){
 
-        int arr[1005][1005] = {0};
-        int dp[1005][1005] = {0};
 
-        int ans = 0;
-        // int max = 0;
-
-        for(int row = 0; row < N; row++){
-            for(int col = 0; col < M; col++){
-                cin >> arr[row][col];
-                dp[row][col] = arr[row][col];
-                ans = max(dp[row][col], ans);
-            }
+        for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= M; j++){
+            cin >> arr[i][j];
+            cache[i][j] = 0;
         }
-
-        for(int row = 1; row < N; row++){
-            for(int col = 1; col < M; col++){
-                if(arr[row][col] == 1){
-                    dp[row][col] = min( dp[row - 1][col], min(dp[row][col - 1], dp[row - 1][col - 1])) + 1;
-                }
-                
-            }
-        }
-
-        for(int row = 0; row < N; row++){
-            for(int col = 0; col < M; col++){
-                ans = max(ans, dp[row][col]);
-            }
-        }
-
-        cout << ans << '\n';
-
-        cin >> N >> M;
-
     }
 
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= M; j++){
+            if(arr[i][j] == 1){
+                cache[i][j] = min( cache[i - 1][j], min(cache[i][j - 1], cache[i - 1][j - 1]) ) + 1;
+            }
+        }
+    }
+
+    int ans = -1;
+
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= M; j++){
+            ans = max(ans, cache[i][j]);
+        }
+    }
+
+    cout << ans << '\n';
+
+    cin >> N >> M;
+    }
+
+    
+
     return 0;
+
 }
