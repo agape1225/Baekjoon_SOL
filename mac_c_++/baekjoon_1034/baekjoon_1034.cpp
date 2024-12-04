5b6fdbeefe370639d7227f3bca481eb10da155fd
agape1225 <49804691+agape1225@users.noreply.github.com>
2024-12-04 16:11:09 +0900

    2024.12.04

--- mac_c_++/baekjoon_1034/baekjoon_1034.cpp
+++ mac_c_++/baekjoon_1034/baekjoon_1034.cpp
@@ -1,56 +1,62 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
-#include <utility>
 
 using namespace std;
 
 int main(void){
-    // int lamps[55][55];
-    vector<string> lamps;
-    int counts[55] = {0};
     int N, M, K;
-    map<string, int> cache;
+    string lamps[55];
     int ans = 0;
+    
+    map<string, int> m;
 
     cin >> N >> M;
 
     for(int i = 0; i < N; i++){
-        string tmp;
-        cin >> tmp;
-        lamps.push_back(tmp);
+        cin >> lamps[i];
+        // for(int j = 0; j < M; j++){
+        //     cin >> lamps[i][j];
+        // }
     }
 
     cin >> K;
 
-    for(auto row: lamps){
+    for(int i = 0; i < N; i++){
+
+        // cout << i << endl;
+
+        //count
         int count = 0;
-        // cout << row << endl;
-        for(auto it : row){
-            if(it == '0'){
+        string pattern = "";
+        for(int j = 0; j < M; j++){
+            if(lamps[i][j] == '0'){
                 count++;
+                pattern+= to_string(j);
             }
         }
 
-        if(count > K || (count % 2) != K % 2){
+        if(count > K || (count % 2 != K % 2)){
             continue;
         }
 
-        if(cache.find(row) == cache.end()){
-            cache.insert(make_pair(row, 1));
+        auto node = m.find(pattern);
+
+        if(node == m.end()){
+            m.insert(make_pair(pattern, 1));
         }else{
-            // cout << row << endl;
-            cache[row]++;
-            // cout << cache.at(row) << endl;
+            node->second++;
+            // m[pattern]++;
         }
     }
 
-    for(auto it : cache){
-        // cout << it.first << ' ' << it.second << endl;
+    for(auto it : m){
         ans = max(ans, it.second);
     }
+
 
     cout << ans;
 
     return 0;
+
 }
