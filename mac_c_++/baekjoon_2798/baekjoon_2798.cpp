58a59078f09e08d12b0ed38c3cd5895db1298c7a
agape1225 <49804691+agape1225@users.noreply.github.com>
2025-03-05 19:58:33 +0900

    2025.03.05

---
+++ mac_c_++/baekjoon_2798/baekjoon_2798.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N , M;
+    int cards[105];
+    int ans = -1;
+    cin >> N >> M;
+
+    for(int i = 0; i < N; i++) {
+        cin >> cards[i];
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            for(int k = 0; k < N; k++) {
+                if(i == j || j == k || i == k){
+                    continue;
+                }
+
+                int sum = cards[i] + cards[j] + cards[k];
+                if(sum <= M) {
+                    // cout << i << ' ' << j << ' ' << k << ' ' << sum << endl;
+                    ans = max(ans, sum);
+                }
+
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
