4963481fcabcf5a81bed03c3bc6987d3cce1ea4b
agape1225 <49804691+agape1225@users.noreply.github.com>
2025-06-07 19:28:27 +0900

    2025.06.07

--- mac_c_++/baekjoon_1106/baekjoon_1106.cpp
+++ mac_c_++/baekjoon_1106/baekjoon_1106.cpp
@@ -1,39 +1,60 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <utility>
+#include <cmath>
 
 using namespace std;
 
+//cache[i][j] -> i개의 도시를 탐색했을 때 최소 j까지 발생하는 비용
+int cache[25][1005] = {0};
+vector<pair<int,int>> cost_info_list;
 
-int cost[21], customer[21];
-int cache[100001] = {0};
+int main(void) {
 
-int max(int x, int y) {
-	return x > y ? x : y;
-}
-
-int main() {
 	int C, N;
-	
+	int cost, number;
+
 	cin >> C >> N;
 
-	for (int i = 1; i <= N; i++)
-		cin >> cost[i] >> customer[i];
-
-	// dp 배열 구하기
-	for (int i = 1; i <= N; i++){
-		for (int j = cost[i]; j <= 100000; j++){
-			cache[j] = max(cache[j], cache[j - cost[i]] + customer[i]);
-		}
-			
+	for(int i = 0; i < N; i++) {
+		cin >> cost >> number;
+		cost_info_list.push_back(
+			make_pair(cost, number)
+		);	
 	}
-		
 
-	// 출력
-	for (int i = 1; i <= 100000; i++)
-		if (cache[i] >= C) {
-			cout << i;
-			break;
+	for(int i = 1; i <= N; i++) {
+		cost = cost_info_list[i - 1].first;
+		number = cost_info_list[i - 1].second;
+
+		for(int j = 1; j <= C; j++) {
+			int real_cost = ceil((j + 0.0) / number) * cost;
+			// cout << ceil((j + 0.0)/ number) << endl;
+			// cout << real_cost << endl;
+
+			if(i == 1) {
+				cache[i][j] = real_cost;
+			}else {
+				if(number <= j) {
+					cache[i][j] = min(cache[i - 1][j], min(cache[i][j - number] + cost, cache[i - 1][j - number] + cost));
+				}else {
+					cache[i][j] = min(cache[i - 1][j], real_cost);
+				}
+			}
+
 		}
-        
+	}
+
+	// for(int i = 0; i <= N; i++) {
+	// 	for(int j = 0; j <= C; j++) {
+	// 		cout << cache[i][j] << ' ';
+	// 	}
+	// 	cout << endl;
+	// }
+
+	cout << cache[N][C];
+
 	return 0;
+
 }
