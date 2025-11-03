a156a35d4bc469eeb8e0ea9f055ae945cc0df29d
agape1225 <49804691+agape1225@users.noreply.github.com>
2025-11-03 21:03:37 +0900

    2025.11.03

--- mac_c_++/baekjoon_1655/baekjoon_1655.cpp
+++ mac_c_++/baekjoon_1655/baekjoon_1655.cpp
@@ -1,43 +1,47 @@
 //1, 2, 3, 4, 5
 
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 #include <queue>
 
 using namespace std;
 
 int main(void){
-    multiset<int> ms;
-    int N, input, mid;
-    auto ans_index = ms.begin();
-    priority_queue<int> max_q;
-    priority_queue<int> min_q;
+
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    cout.tie(0);
+
+    priority_queue<int> max_queue;
+    priority_queue<int> min_queue;
+    int N, input;
 
     cin >> N;
 
     for(int i = 0; i < N; i++){
+
         cin >> input;
 
-        if(i == 0){
-            // mid = input;
-            max_q.push(input);
-        }else{
-
-            int mid = max_q.top();
-
-            if(mid < input){
-                min_q.push(-input);
-
-            }else{
-                max_q.push(input);
-            }
-
-            if(max_q.size() <= min_q.size()){
-                
-            }
-
+        if(min_queue.size() == max_queue.size()) {
+            max_queue.push(input);
+        }else {
+            min_queue.push(-input);
         }
-        cout << max_q.top() << '\n';
+
+        if(!min_queue.empty() && !max_queue.empty() && max_queue.top() > -min_queue.top()) {
+            int min_top = -min_queue.top();
+            int max_top = max_queue.top();
+
+            max_queue.pop();
+            min_queue.pop();
+
+            max_queue.push(min_top);
+            min_queue.push(-max_top);
+        }
+
+        cout << max_queue.top() << '\n';
+        
     }
 
     return 0;
