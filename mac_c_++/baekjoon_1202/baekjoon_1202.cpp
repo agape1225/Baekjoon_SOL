0dfc464fb809a4cb7cd858d4eb16a62a25b21dfd
agape1225 <49804691+agape1225@users.noreply.github.com>
2025-11-09 21:18:38 +0900

    2025.11.09

--- mac_c_++/baekjoon_1202/baekjoon_1202.cpp
+++ mac_c_++/baekjoon_1202/baekjoon_1202.cpp
@@ -1,98 +1,63 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 #include <queue>
+#include <vector>
 
 using namespace std;
-
-struct  comp
-{
-    bool operator()(pair<int, int> n1, pair<int, int> n2) {
-        if(n1.second == n2.second) {
-            return n1.first < n2.first;
-        }
-        return n1.second < n2.second;
-    }
-};
 
 bool comp1(pair<int, int> n1, pair<int, int> n2) {
     return n1.first < n2.first;
 }
 
-bool comp2(int a, int b) {
-    return a > b;
-}
-
-
 int main(void) {
-    priority_queue<int> pq;
-    vector<int> bags;
-    vector<pair<int, int>> j;
-    int N, M;
-    int answer = 0;
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    cout.tie(0);
 
-    cin >> N >> M;
+    vector<int> bags;
+    vector<pair<int, int>> js;
+    priority_queue<int> pq; 
+    long long ans = 0;
+
+    int weight, value, space;
+    int N, K;
+
+    cin >> N >> K;
 
     for(int i = 0; i < N; i++) {
-        int a, b;
-        cin >> a >> b;
-        // pq.push(make_pair(a, b));
-        j.push_back(make_pair(a, b));
+        cin >> weight >> value;
+        js.push_back(make_pair(weight, value));
     }
 
-    for(int i = 0; i < M; i++) {
-        int a;
-        cin >> a;
-        bags.push_back(a);
+    for(int i = 0; i < K; i++) {
+        cin >> space;
+        bags.push_back(space);
     }
 
-    sort(j.begin(), j.end(), comp1);
+    sort(js.begin(), js.end(), comp1);
     sort(bags.begin(), bags.end());
 
-    
-
-    int bag_index = 0;
-
-    // for(int i = 0; i < N; i++) {
-    //     cout << j[i].first << ' ' << j[i].second << endl;
+    // for(auto it : js) {
+    //     cout << it.first << ' ' << it.second << endl;
     // }
 
     int j_index = 0;
-
-    for(int i = 0; i < M; i++) {
+    for(int i = 0; i < K; i++) {
         int bag_size = bags[i];
 
-        while(j_index < N && j[j_index].first <= bag_size) {
-            // cout << j[j_index].first << ' ' << bag_size << endl;
-            pq.push(j[j_index].second);
+        while(j_index < N && js[j_index].first <= bag_size) {
+            pq.push(js[j_index].second);
             j_index++;
         }
 
         if(!pq.empty()) {
-            answer += pq.top();
+            ans += pq.top();
             pq.pop();
         }
 
     }
-    // while(!pq.empty() && bag_index < M) {
-    //     int bag_size = bags[bag_index++];
-    //     while(!pq.empty() && bag_size < pq.top().first) {
-    //         pq.pop();
-    //     }
-
-    //     if(!pq.empty()) {
-    //         answer += pq.top().second;
-    //         pq.pop();
-    //     }
-    // }
-
-    cout << answer;
-
-    // while(!pq.empty()) {
-    //     cout << pq.top().first << ' ' << pq.top().second << endl;
-    //     pq.pop();
-    // }
+    
+    cout << ans;
 
     return 0;
-
 }
--- mac_c_++/baekjoon_1655/baekjoon_1655.cpp
+++ mac_c_++/baekjoon_1655/baekjoon_1655.cpp
@@ -1,47 +1,42 @@
-//1, 2, 3, 4, 5
-
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <queue>
 
 using namespace std;
 
-int main(void){
-
-    ios::sync_with_stdio(false);
-	cin.tie(0);
-    cout.tie(0);
-
-    priority_queue<int> max_queue;
-    priority_queue<int> min_queue;
+int main(void) {
     int N, input;
+    priority_queue<int> max_q;
+    priority_queue<int> min_q;
 
     cin >> N;
 
-    for(int i = 0; i < N; i++){
-
+    for(int i = 0; i < N; i++) {
         cin >> input;
 
-        if(min_queue.size() == max_queue.size()) {
-            max_queue.push(input);
+        if(max_q.size() == min_q.size()) {
+            // cout << "max " << input << endl; 
+            max_q.push(input);
+            
         }else {
-            min_queue.push(-input);
+            // cout << "min " << input << endl;
+            min_q.push(-input);
         }
 
-        if(!min_queue.empty() && !max_queue.empty() && max_queue.top() > -min_queue.top()) {
-            int min_top = -min_queue.top();
-            int max_top = max_queue.top();
+        if(!max_q.empty() && !min_q.empty() && (max_q.top() > -min_q.top())) {
+            int min_num = -min_q.top();
+            int max_num = max_q.top();
 
-            max_queue.pop();
-            min_queue.pop();
+            // cout << min_num << ' ' << max_num << endl;
 
-            max_queue.push(min_top);
-            min_queue.push(-max_top);
+            min_q.pop();
+            max_q.pop();
+
+            min_q.push(-max_num);
+            max_q.push(min_num);
         }
 
-        cout << max_queue.top() << '\n';
-        
+        cout << max_q.top() << '\n';
     }
 
     return 0;
