f0300d609c8698c366daa5d452652c28480bf23b
agape1225 <49804691+agape1225@users.noreply.github.com>
2025-12-18 21:00:40 +0900

    2025.12.18

--- mac_c_++/baekjoon_11003/baekjoon_11003.cpp
+++ mac_c_++/baekjoon_11003/baekjoon_11003.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
-#include <queue>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int main(void) {
 
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
-    int N, L, tmp;
+    int N, L, input;
     vector<int> arr;
     deque<int> dq;
-    
+
     cin >> N >> L;
 
     for(int i = 0; i < N; i++) {
-        cin >> tmp;
-        arr.push_back(tmp);
+        cin >> input;
+        arr.push_back(input);
     }
 
     for(int i = 0; i < N; i++) {
-        int last_index = i - L + 1;
-
-        while(!dq.empty() && dq.front() < last_index) {
+        int num = arr[i];
+        while(!dq.empty() && dq.front() <= i - L) {
             dq.pop_front();
         }
-        
-        while(!dq.empty() && arr[dq.back()] > arr[i]) {
+
+        while(!dq.empty() && arr[dq.back()] > num) {
             dq.pop_back();
         }
+
         dq.push_back(i);
+
         cout << arr[dq.front()] << ' ';
     }
 
